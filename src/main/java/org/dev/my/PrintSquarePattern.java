f38d02ad7413a0e0c756d54b49652b35c60bb93b
Jεan Sacren <sakiwit@gmail.com>
2018-08-04 02:29:34 -0600

    Add PrintSquarePattern

---
+++ src/main/java/org/dev/my/PrintSquarePattern.java
@@ -0,0 +1,57 @@
+/**
+ * Print the square pattern.
+ *
+ * <p>
+ *   * * * * *
+ *   *       *
+ *   *       *
+ *   *       *
+ *   * * * * *
+ * </p>
+ */
+public class PrintSquarePattern {
+  /**
+   * Entry point.
+   */
+  public static void main(String[] args) {
+    printSquarePattern();
+  }
+
+  private static void printSquarePattern() {
+    System.out.println();
+    printThreeSpaces();
+    printFiveStars();
+
+    for (int i = 0; i < 3; i++) {
+      printThreeSpaces();
+      printTwoStars();
+    }
+
+    printThreeSpaces();
+    printFiveStars();
+  }
+
+  private static void printFiveStars() {
+    for (int i = 0; i < 4; i++) {
+      System.out.print("* ");
+    }
+
+    System.out.println("*");
+  }
+
+  private static void printTwoStars() {
+    System.out.print("*");
+
+    for (int i = 0; i < 7; i++) {
+      System.out.print(" ");
+    }
+
+    System.out.println("*");
+  }
+
+  private static void printThreeSpaces() {
+    for (int i = 0; i < 3; i++) {
+      System.out.print(" ");
+    }
+  }
+}
