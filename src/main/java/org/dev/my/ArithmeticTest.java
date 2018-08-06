19a9ce4295a9a3f1cf14ab571b52c4de383e2a9c
Jεan Sacren <sakiwit@gmail.com>
2018-08-06 03:41:53 -0600

    Add ArithmeticTest class

---
+++ src/main/java/org/dev/my/ArithmeticTest.java
@@ -0,0 +1,25 @@
+/**
+ * Test arithmetic operations.
+ */
+public class ArithmeticTest {
+  public static void main(String[] args) {
+    testArithmetic();
+  }
+
+  private static void testArithmetic() {
+    int number1 = 98;
+    int number2 = 5;
+    int sum = number1 + number2;
+    int difference = number1 - number2;
+    int product = number1 * number2;
+    int quotient = number1 / number2;
+    int remainder = number1 % number2;
+
+    System.out.println("The sum, difference, product, quotient and remainder:\n"
+                       + "98 + 5 = " + sum + "\n"
+                       + "98 - 5 = " + difference + "\n"
+                       + "98 × 5 = " + product + "\n"
+                       + "98 ÷ 5 = " + quotient + "\n"
+                       + "98 % 5 = " + remainder);
+  }
+}
