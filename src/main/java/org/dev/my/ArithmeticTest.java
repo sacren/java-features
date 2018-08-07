de774675f0baf2af09b7f54f404d56d7404e4122
Jεan Sacren <sakiwit@gmail.com>
2018-08-07 01:51:18 -0600

    Add sum of multiplication

--- src/main/java/org/dev/my/ArithmeticTest.java
+++ src/main/java/org/dev/my/ArithmeticTest.java
@@ -14,12 +14,14 @@ public class ArithmeticTest {
     int product = number1 * number2;
     int quotient = number1 / number2;
     int remainder = number1 % number2;
+    int multiSum = number1 * 31 + number2 * 17;
 
     System.out.println("The sum, difference, product, quotient and remainder:\n"
                        + "98 + 5 = " + sum + "\n"
                        + "98 - 5 = " + difference + "\n"
                        + "98 × 5 = " + product + "\n"
                        + "98 ÷ 5 = " + quotient + "\n"
-                       + "98 % 5 = " + remainder);
+                       + "98 % 5 = " + remainder + "\n\n"
+                       + "98 × 31 + 5 × 17 = " + multiSum);
   }
 }
