de8cb38982442f575c2606a233c7ef03f9786c89
Jεan Sacren <sakiwit@gmail.com>
2018-08-09 02:40:01 -0600

    Add RunningNumberDivisible class

---
+++ src/main/java/org/dev/my/RunningNumberDivisible.java
@@ -0,0 +1,26 @@
+/**
+ * Sum all the integers between 1 and 1000, that are divisible by 13, 15 or 17,
+ * but not by 30.
+ */
+public class RunningNumberDivisible {
+  public static void main(String[] args) {
+    sumNumbersDivisible();
+  }
+
+  private static void sumNumbersDivisible() {
+    int upperBound = 1000;
+    int lowerBound = 1;
+    int sum = 0;
+
+    for (int i = lowerBound; i <= upperBound; i++) {
+      if ((i % 13 == 0
+           || i % 15 == 0
+           || i % 17 == 0)
+          && i % 30 != 0) {
+        sum += i;
+      }
+    }
+
+    System.out.println("Sum of integers divisible is " + sum + ".");
+  }
+}
