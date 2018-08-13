737392b35721906cb8334de9a3408922fb6d9b6c
Jεan Sacren <sakiwit@gmail.com>
2018-08-13 01:33:30 -0600

    Add StudentInfo class

---
+++ src/main/java/org/dev/my/StudentInfo.java
@@ -0,0 +1,24 @@
+/**
+ * Mangae student info for a college.
+ */
+public class StudentInfo {
+  public static void main(String[] args) {
+    setStudentInfo();
+  }
+
+  private static void setStudentInfo() {
+    String name;
+    String address;
+    String phoneNumber;
+    char gender;
+    String dateOfBirth;
+    double height;
+    double weight;
+    String degree;
+    int yearOfStudy;
+    double averageGpa;
+    boolean tuitionGrant;
+    boolean scholar;
+    int studentId;
+  }
+}
