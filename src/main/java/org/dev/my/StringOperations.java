82952db05fd549d1b898ea036e77af2191c4165e
Jεan Sacren <sakiwit@gmail.com>
2018-08-15 02:48:44 -0600

    Add more String methods

--- src/main/java/org/dev/my/StringOperations.java
+++ src/main/java/org/dev/my/StringOperations.java
@@ -23,6 +23,14 @@ public class StringOperations {
                        + s1 + " = " + s2 + "? " + s1.equals(s2) + "\n"
                        + s1 + " = " + s2 + "? (case ignored) " + s1.equalsIgnoreCase(s2) + "\n\n"
                        + s2 + " = " + s1 + "? " + s2.equals(s1) + "\n"
-                       + s2 + " = " + s1 + "? (case ignored) " + s2.equalsIgnoreCase(s1));
+                       + s2 + " = " + s1 + "? (case ignored) " + s2.equalsIgnoreCase(s1) + "\n\n"
+                       + "Substring of " + s1 + " = " + s1.substring(middle) + "\n"
+                       + "Substring of " + s2 + " = " + s2.substring(0, length - middle) + "\n"
+                       + "Index (o) of " + s2 + " = " + s2.indexOf('o') + "\n"
+                       + "Last index (o) of " + s2 + " = " + s2.lastIndexOf('o') + "\n\n"
+                       + "Ends with -ity of " + s2 + " = " + s2.endsWith("ity") + "\n"
+                       + "Ends with -aty of " + s2 + " = " + s2.endsWith("aty") + "\n\n"
+                       + "Upper case of " + s2 + " = " + s2.toUpperCase() + "\n"
+                       + "Lower case of " + s1 + " = " + s1.toLowerCase());
   }
 }
