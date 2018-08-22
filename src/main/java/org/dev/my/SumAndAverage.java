9c95d79f31401ddee5d3a9dd0b9e98d896c96dc2
Jεan Sacren <sakiwit@gmail.com>
2018-08-22 02:50:39 -0600

    Add sumOddNumbersAndAverage() method

--- src/main/java/org/dev/my/SumAndAverage.java
+++ src/main/java/org/dev/my/SumAndAverage.java
@@ -1,9 +1,12 @@
 /**
  * Sum all the numbers from 111 to 8899.
+ *
+ * Sum only the odd numbers from 1 to 100 and compute the average.
  */
 public class SumAndAverage {
   public static void main(String[] args) {
     sumNumbersAndAverage();
+    sumOddNumbersAndAverage();
   }
 
   private static void sumNumbersAndAverage() {
@@ -22,5 +25,25 @@ public class SumAndAverage {
 
     System.out.println("Sum from 111 to 8899 is " + sum + ".\n"
                        + "Average is " + average + ".");
+  }
+
+  private static void sumOddNumbersAndAverage() {
+    int upperBound = 100;
+    int lowerBound = 1;
+    int sum = 0;
+    int count = 0;
+    double average;
+
+    for (int i = lowerBound; i <= upperBound; i++) {
+      if (i % 2 == 1) {
+        sum += i;
+        count++;
+      }
+    }
+
+    average = (double)sum / count;
+
+    System.out.println("Sum of odd numbers is " + sum + ".\n"
+                       + "Average is " + average);
   }
 }
