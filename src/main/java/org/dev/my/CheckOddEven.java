c4f6d7ad9d4d9a958bb75d216b4c0ca3c1536d64
Jεan Sacren <sakiwit@gmail.com>
2018-08-29 01:06:53 -0600

    Add printOddEven() method
    Move the printing procedure out of isOddOrEven() method.

--- src/main/java/org/dev/my/CheckOddEven.java
+++ src/main/java/org/dev/my/CheckOddEven.java
@@ -3,11 +3,22 @@
  */
 public class CheckOddEven {
   public static void main(String[] args) {
-    isOddOrEven();
+    printOddEven();
   }
 
-  private static void isOddOrEven() {
-    int number = 49;
+  private static void printOddEven() {
+    int n1 = 0;
+    int n2 = 49;
+    final int n3 = 1028;
+    final int n4 = -1;
+
+    isOddOrEven(n1);
+    isOddOrEven(n2);
+    isOddOrEven(n3);
+    isOddOrEven(n4);
+  }
+
+  private static void isOddOrEven(int number) {
     int remainder = number % 2;
 
     if (remainder == 0) {
