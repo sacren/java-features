19e0f7efb93f651979896ae596424d6e824c116a
Jεan Sacren <sakiwit@gmail.com>
2018-08-30 01:38:56 -0600

    Add SquareBoard class

---
+++ src/main/java/org/dev/my/SquareBoard.java
@@ -0,0 +1,29 @@
+/**
+ * Print 5 × 5 pattern using two nested loops.
+ * <p>
+ * # # # # #
+ * # # # # #
+ * # # # # #
+ * # # # # #
+ * # # # # #
+ * </p>
+ */
+public class SquareBoard {
+  public static void main(String[] args) {
+    printSquareBoard();
+  }
+
+  private static void printSquareBoard() {
+    final int size = 5;
+
+    for (int row = 1; row <= size; row++) {
+      for (int col = 1; col <= size; col++) {
+        if (col == size) {
+          System.out.println('#');
+          break;
+        }
+        System.out.print("# ");
+      }
+    }
+  }
+}
