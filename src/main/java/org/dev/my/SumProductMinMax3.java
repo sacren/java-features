56039741f8237f06d0b04913244fc1f013f6c4f6
Jεan Sacren <sakiwit@gmail.com>
2018-09-14 03:35:47 -0600

    Add calculation and comparison of user input

--- src/main/java/org/dev/my/SumProductMinMax3.java
+++ src/main/java/org/dev/my/SumProductMinMax3.java
@@ -2,20 +2,52 @@
  * Prompt the user for three integers and compute the sum, product, minimum and
  * maximum.
  */
+import java.util.Scanner;
+
 public class SumProductMinMax3 {
   public static void main(String[] args) {
     compute3Numbers();
   }
 
   private static void compute3Numbers() {
-    int number1 = 98;
-    int number2 = 5;
-    int number3 = 77;
-    int sum = number1 + number2 + number3;
-    int product = number1 * number2 * number3;
+    Scanner inputStream = new Scanner(System.in);
+    System.out.print("Enter the first integer: ");
+    int number1 = inputStream.nextInt();
+    System.out.print("Enter the second integer: ");
+    int number2 = inputStream.nextInt();
+    System.out.print("Enter the third integer: ");
+    int number3 = inputStream.nextInt();
 
-    System.out.println("The sum and product of three numbers:\n"
-                       + "98 + 5 + 77 = " + sum + "\n"
-                       + "98 × 5 × 77 = " + product);
+    final int sum = number1 + number2 + number3;
+    final int product = number1 * number2 * number3;
+    int max;
+    int min;
+
+    if (number1 >= number2) {
+      max = number1;
+      min = number2;
+    } else {
+      max = number2;
+      min = number1;
+    }
+
+    if (max < number3) {
+      max = number3;
+    }
+
+    if (min > number3) {
+      min = number3;
+    }
+
+    System.out.printf("The sum of %d, %d and %d is %d%n"
+                      + "The product of %d, %d and %d is %d%n"
+                      + "The max of %d, %d and %d is %d%n"
+                      + "The min of %d, %d and %d is %d%n",
+                      number1, number2, number3, sum,
+                      number1, number2, number3, product,
+                      number1, number2, number3, max,
+                      number1, number2, number3, min);
+
+    inputStream.close();
   }
 }
