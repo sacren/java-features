a52d6a511570bf060bc65b111e1d6ef3ade2876d
Jεan Sacren <sakiwit@gmail.com>
2018-09-15 02:22:10 -0600

    Add calculation and comparison of 5 integers

--- src/main/java/org/dev/my/SumProductMinMax5.java
+++ src/main/java/org/dev/my/SumProductMinMax5.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 public class SumProductMinMax5 {
   public static void main(String[] args) {
-    compute3Numbers();
+    compute5Numbers();
   }
 
-  private static void compute3Numbers() {
+  private static void compute5Numbers() {
     Scanner inputStream = new Scanner(System.in);
     System.out.print("Enter the first integer: ");
     int number1 = inputStream.nextInt();
@@ -17,9 +17,13 @@ public class SumProductMinMax5 {
     int number2 = inputStream.nextInt();
     System.out.print("Enter the third integer: ");
     int number3 = inputStream.nextInt();
+    System.out.print("Enter the fourth integer: ");
+    int number4 = inputStream.nextInt();
+    System.out.print("Enter the fifth integer: ");
+    int number5 = inputStream.nextInt();
 
-    final int sum = number1 + number2 + number3;
-    final int product = number1 * number2 * number3;
+    final int sum = number1 + number2 + number3 + number4 + number5;
+    final int product = number1 * number2 * number3 * number4 * number5;
     int max;
     int min;
 
@@ -35,18 +39,34 @@ public class SumProductMinMax5 {
       max = number3;
     }
 
+    if (max < number4) {
+      max = number4;
+    }
+
+    if (max < number5) {
+      max = number5;
+    }
+
     if (min > number3) {
       min = number3;
     }
 
-    System.out.printf("The sum of %d, %d and %d is %d%n"
-                      + "The product of %d, %d and %d is %d%n"
-                      + "The max of %d, %d and %d is %d%n"
-                      + "The min of %d, %d and %d is %d%n",
-                      number1, number2, number3, sum,
-                      number1, number2, number3, product,
-                      number1, number2, number3, max,
-                      number1, number2, number3, min);
+    if (min > number4) {
+      min = number4;
+    }
+
+    if (min > number5) {
+      min = number5;
+    }
+
+    System.out.printf("The sum of %d, %d, %d, %d and %d is %d%n"
+                      + "The product of %d, %d, %d, %d and %d is %d%n"
+                      + "The max of %d, %d, %d, %d and %d is %d%n"
+                      + "The min of %d, %d, %d, %d and %d is %d%n",
+                      number1, number2, number3, number4, number5, sum,
+                      number1, number2, number3, number4, number5, product,
+                      number1, number2, number3, number4, number5, max,
+                      number1, number2, number3, number4, number5, min);
 
     inputStream.close();
   }
