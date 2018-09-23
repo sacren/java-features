a843985c833e991a8705f3bffe6ec19db33fc860
Jεan Sacren <sakiwit@gmail.com>
2018-09-23 02:25:48 -0600

    Add SalesTaxCalculator class

---
+++ src/main/java/org/dev/my/SalesTaxCalculator.java
@@ -0,0 +1,35 @@
+/**
+ * Continuously input the tax-inclusive price of 7% tax rate.  Compute the
+ * actual price and the sales tax rounded to 2 decimal places.  Print the total
+ * price, the actual price and sales tax when terminate.
+ */
+import java.util.Scanner;
+
+public class SalesTaxCalculator {
+  /**
+   * Entry point to SalesTaxCalculator class.
+   */
+  public static void main(String[] args) {
+    double taxInPrice;
+    Scanner inputStream = new Scanner(System.in);
+
+    System.out.print("Enter the price including sales tax: ");
+    taxInPrice = inputStream.nextDouble();
+    calculatePrice(taxInPrice);
+
+    inputStream.close();
+  }
+
+  /**
+   * Calculate actual price and sales tax.
+   */
+  public static void calculatePrice(double userInput) {
+    double taxRate = 0.07;
+    double actualPrice = userInput / (1 + taxRate);
+    double salesTax = actualPrice * taxRate;
+    System.out.printf("The actual price is %.2f%n"
+                      + "The sales tax is %.2f%n"
+                      + "The total price is %.2f%n",
+                      actualPrice, salesTax, userInput);
+  }
+}
