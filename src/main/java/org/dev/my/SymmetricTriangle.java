051a50379d998dce120da638fc9b8e32196222ad
Jεan Sacren <sakiwit@gmail.com>
2018-10-10 01:00:04 -0600

    Add user input for a positive odd number

--- src/main/java/org/dev/my/SymmetricTriangle.java
+++ src/main/java/org/dev/my/SymmetricTriangle.java
@@ -15,9 +15,30 @@
  *   * * * * *
  *      (b)
  */
+import java.util.Scanner;
+
 public class SymmetricTriangle {
+  /**
+   * Get size of the triangle from the user.
+   */
   public static void main(String[] args) {
+    int size;
+
+    Scanner inStream = new Scanner(System.in);
+
+    for (;;) {
+      System.out.print("Enter positive odd number: ");
+      size = inStream.nextInt();
+
+      if (size > 0 && size % 2 == 1) {
+        break;
+      }
+
+      System.out.println("You have entered an invalid number. Try again.");
+    }
+
     printTrianglePattern();
+    inStream.close();
   }
 
   private static void printTrianglePattern() {
