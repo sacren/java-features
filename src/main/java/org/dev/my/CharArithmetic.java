c5689f395387a8d3b9f38283603ca42b1cdc1d90
Jεan Sacren <sakiwit@gmail.com>
2018-10-13 01:33:52 -0600

    Add more char operation and comments

--- src/main/java/org/dev/my/CharArithmetic.java
+++ src/main/java/org/dev/my/CharArithmetic.java
@@ -10,24 +10,26 @@ public class CharArithmetic {
    * The type char in action.
    */
   public static void runChar() {
-    final char c1 = '0';
+    final char c1 = '0';        /* Declare char and initialize with char */
     final char c2 = 'A';
-    char c3;
-    char c4;
+    char c3 = 98;               /* Declare char and initilize with int */
     int n1;
     int n2;
 
-    c3 = 98;              /* Remain char */
-    c4 = (char)(c3 - 1);  /* Promote to int for calculation */
     c3 -= 44;             /* Compound operation of char */
     n1 = c3 - '0';        /* Convert '0'..'9' to 0..9 */
-    n2 = c4 - c1;         /* Same as above */
-    c3 += 44;             /* Compound operation of char */
+    n2 = c3 - c1;         /* Same as above */
+    c3 += 44;
 
+    char c4 = 75;
+    c4 = (char)(c4 - 1);  /* Promote to int for calculation */
+
+    /* Increment of char */
     for (char c = 'a'; c < 'd'; c++) {
       System.out.println(c);
     }
 
+    /* Decrement of char */
     for (char c = 'Z'; c > 'W'; c--) {
       System.out.println(c);
     }
@@ -52,11 +54,11 @@ public class CharArithmetic {
                       + "%c%n"    /* Display 'A' by literal char */
                       + "%c%n"    /* Display 'b' by char */
                       + "%c%n"    /* Display 'b' by casting char to int */
-                      + "%c%n"    /* Display 'c' by literal int */
-                      + "%c%n%n"  /* Display 'a' by int operation */
+                      + "%c%n"    /* Display 'J' by literal int */
+                      + "%c%n%n"  /* Display 'J' by int operation */
                       + "Now with cast:%n"
                       + "%d%n"    /* Display integer casting from char */
                       + "%d%n",   /* Display integer casting from char */
-                      c1, c2, 'A', c3, (int)c3, 99, c4, (int)c1, (int)c2);
+                      c1, c2, 'A', c3, (int)c3, 74, c4, (int)c1, (int)c2);
   }
 }
