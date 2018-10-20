97f6a8f317ca632f316ed5590d7836c401515115
Jεan Sacren <sakiwit@gmail.com>
2018-10-20 01:28:25 -0600

    Add convertToDecimal() method

--- src/main/java/org/dev/my/HexString.java
+++ src/main/java/org/dev/my/HexString.java
@@ -25,7 +25,9 @@ public class HexString {
     }
 
     if (isHex(line)) {
-      System.out.printf("%s is a hex string%n", line);
+      System.out.printf("%s is a hex string%n"
+                        + "%1$s in decimal is %d%n",
+                        line, convertToDecimal(line));
     } else {
       System.out.printf("%s is not a hex string%n", line);
     }
@@ -57,5 +59,40 @@ public class HexString {
     }
 
     return false;
+  }
+
+  private static int convertToDecimal(String s) {
+    final int size = s.length();
+    final int first = 0;
+    int decimal = 0;
+    int digit;
+    int exponent;
+    char c;
+
+    for (int i = first; i < size; i++) {
+      c = s.charAt(i);
+      exponent = size - i - 1;
+      digit = Character.digit(c, 10);
+
+      for (;;) {
+        if (digit >= 0 && digit <= 9) {
+          break;
+        }
+
+        if (Character.compare(c, 'a') >= 0 && Character.compare(c, 'f') <= 0) {
+          digit = c - 'a' + 10;
+          break;
+        }
+
+        if (Character.compare(c, 'A') >= 0 && Character.compare(c, 'F') <= 0) {
+          digit = c - 'A' + 10;
+          break;
+        }
+      }
+
+      decimal += (int)Math.pow(16, exponent) * digit;
+    }
+
+    return decimal;
   }
 }
