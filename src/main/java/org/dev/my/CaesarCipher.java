291ab1d5f7a38b6f21a5ffd65eea6bcfc059f903
Jεan Sacren <sakiwit@gmail.com>
2018-10-24 01:07:10 -0600

    Add CaesarCipher class

---
+++ src/main/java/org/dev/my/CaesarCipher.java
@@ -0,0 +1,87 @@
+/**
+ * Caesar's Cipher is one of the simplest encryption techniques.  Each letter
+ * in the plaintext is replaced by a letter some fixed number of position (n)
+ * down the alphabet cyclically.  Prompt user for some plain text consisting of
+ * mixed-case letters only.  Construct the cipher text and print it out in
+ * uppercase.  Let n = 3.
+ */
+import java.util.Scanner;
+
+public class CaesarCipher {
+  /**
+   * Entry point of the class with validation of user input.
+   */
+  public static void main(String[] args) {
+    String line;
+
+    Scanner inStream = new Scanner(System.in);
+
+    for (;;) {
+      System.out.print("Enter text in mixed case: ");
+      line = inStream.nextLine();
+
+      if (line.isEmpty()) {
+        System.out.println("The string is empty. Try again.");
+        continue;
+      }
+
+      if (isInputValid(line)) {
+        break;
+      }
+
+      System.out.println("You entered an invalid string. Try again.");
+    }
+
+    System.out.printf("The cipher text: %s%n", mkCipherText(line));
+
+    inStream.close();
+  }
+
+  private static boolean isInputValid(String s) {
+    final int size = s.length();
+    final int first = 0;
+
+    for (int i = first; i < size; i++) {
+      if (!Character.isLetter(s.charAt(i))) {
+        break;
+      }
+
+      if (i == size - 1) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private static String mkCipherText(String s) {
+    final int size = s.length();
+    final int first = 0;
+    final int key = 3;
+    String cipherText = new String();
+    char c;
+
+    s = s.toUpperCase();
+
+    for (int i = first; i < size; i++) {
+      c = s.charAt(i);
+
+      switch (c) {
+        case 'X':
+          cipherText += "A";
+          break;
+        case 'Y':
+          cipherText += "B";
+          break;
+        case 'Z':
+          cipherText += "C";
+          break;
+        default:
+          c = (char)(c + key);
+          cipherText += Character.toString(c);
+      }
+    }
+
+    return cipherText;
+  }
+}
