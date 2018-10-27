2a366aae9d1cad9c8df5f11030aa45ee635cacc5
Jεan Sacren <sakiwit@gmail.com>
2018-10-27 02:36:17 -0600

    Add sum of six integers

--- src/main/java/org/dev/my/SixNumberProduct.java
+++ src/main/java/org/dev/my/SixNumberProduct.java
@@ -1,8 +1,25 @@
-/** Multiplication of six integers. */
+/** Addition and multiplication of six integers. */
 public class SixNumberProduct {
-    /** Print out the product of six integers. */
+    /** Print out the sum and product of six integers. */
     public static void main(String[] args) {
-        System.out.printf("The product of 11, 22, 33, 44, 55 and 66 is %d%n", productOfSix());
+        System.out.printf(
+                "The sum of 11, 22, 33, 44, 55 and 66 is %d%n"
+                + "The product of 11, 22, 33, 44, 55 and 66 is %d%n",
+                sumOfSix(), productOfSix());
+    }
+
+    /* Calculate the sum of six integers. */
+    private static int sumOfSix() {
+        int[] numbers = {11, 22, 33, 44, 55, 66};
+        int sum = 0;
+        final int first = 0;
+        final int size = numbers.length;
+
+        for (int i = first; i < size; i++) {
+            sum += numbers[i];
+        }
+
+        return sum;
     }
 
     /* Calculate the product of six integers. */
