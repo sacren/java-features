75644bfc79c9edba6e1fb61d78ce0ad3953403e8
Jεan Sacren <sakiwit@gmail.com>
2018-10-29 01:38:47 -0600

    Add user interaction for day of the week

--- src/main/java/org/dev/my/PrintDayInWord.java
+++ src/main/java/org/dev/my/PrintDayInWord.java
@@ -1,40 +1,53 @@
 /** Print day of the week. */
+import java.util.Scanner;
+
 public class PrintDayInWord {
+    /** Ask the user for a number to show day of the week. */
     public static void main(String[] args) {
-        printDay();
-    }
+        int day;
 
-    private static void printDay() {
-        int day = 2;
-        String word = "Not a valid day";
+        Scanner inStream = new Scanner(System.in);
 
-        switch (day) {
-            case 0:
-                word = "Sunday";
+        for ( ; ; ) {
+            System.out.print("Enter between 0 and 6 with 0 for Sunday: ");
+            day = inStream.nextInt();
+
+            if (day >= 0 && day <= 6) {
                 break;
-            case 1:
-                word = "Monday";
-                break;
-            case 2:
-                word = "Tuesday";
-                break;
-            case 3:
-                word = "Wednesday";
-                break;
-            case 4:
-                word = "Thursday";
-                break;
-            case 5:
-                word = "Friday";
-                break;
-            case 6:
-                word = "Saturday";
-                break;
-            default:
-                word = "Not a valid day";
-                break;
+            }
+
+            System.out.println("You entered an invalid number. Try again.");
         }
 
-        System.out.println(word);
+        dayOfWeek(day);
+        inStream.close();
+    }
+
+    private static void dayOfWeek(int number) {
+        String day = "Sunday";
+
+        switch (number) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            default:
+        }
+
+        System.out.printf("You entered %d for %s%n", number, day);
     }
 }
