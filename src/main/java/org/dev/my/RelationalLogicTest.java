653539b502c57c167d788223a026add339f724cd
Jεan Sacren <sakiwit@gmail.com>
2018-11-01 04:43:54 -0600

    Add final modifier to variables

--- src/main/java/org/dev/my/RelationalLogicTest.java
+++ src/main/java/org/dev/my/RelationalLogicTest.java
@@ -5,12 +5,12 @@ public class RelationalLogicTest {
     }
 
     private static void printRelationalLogic() {
-        int age = 18;
-        double weight = 71.23;
-        double height = 191;
-        boolean married = false;
-        boolean attached = false;
-        char gender = 'm';
+        final int age = 18;
+        final double weight = 71.23;
+        final double height = 191;
+        final boolean married = false;
+        final boolean attached = false;
+        final char gender = 'm';
 
         System.out.println(
                 (age == 18 && !married && !attached && gender == 'm')
