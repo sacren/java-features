0a755af5c8023d840322c14d65bf6aca47420447
Jεan Sacren <sakiwit@gmail.com>
2018-11-03 04:41:07 -0600

    Refactor Add2Integer class

--- src/main/java/org/dev/my/Add2Integer.java
+++ src/main/java/org/dev/my/Add2Integer.java
@@ -1,19 +1,19 @@
-/** Prompt user for two integers and print their sum. */
+/** Prompt user for two integers and print the sum. */
 import java.util.Scanner;
 
 public class Add2Integer {
+    /** Prompt the user for two integers and print the sum. */
     public static void main(String[] args) {
-        add2Integer();
-    }
-
-    private static void add2Integer() {
-        Scanner stream = new Scanner(System.in);
+        Scanner inStream = new Scanner(System.in);
 
         System.out.print("Enter the first integer: ");
-        int n1 = stream.nextInt();
+        final int n1 = inStream.nextInt();
         System.out.print("Enter the second integer: ");
-        int n2 = stream.nextInt();
-        stream.close();
-        System.out.printf("The sum of %d and %d is %d.%n", n1, n2, n1 + n2);
+        final int n2 = inStream.nextInt();
+        System.out.printf(
+                "The sum of %d and %d is %d%n",
+                n1, n2, n1 + n2);
+
+        inStream.close();
     }
 }
