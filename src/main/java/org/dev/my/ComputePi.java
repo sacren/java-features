6fd55684194c94a74e7aa5c738fdeb153adba639
Jεan Sacren <sakiwit@gmail.com>
2018-11-23 04:35:28 -0700

    Update PI calculation method
    Move the printout out of the method and let it return double type.  Use
    printf() for output.

--- src/main/java/org/dev/my/ComputePi.java
+++ src/main/java/org/dev/my/ComputePi.java
@@ -4,41 +4,31 @@
  * <p>π = 4 × (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ...)
  */
 public class ComputePi {
+    /** Display the value of π. */
     public static void main(String[] args) {
-        printPi();
+        final double pi = getPi();
+        System.out.printf(
+                "π = %.15f%n"
+                        + "π of Math.PI = %.15f%n"
+                        + "Percent of Math.PI = %.15f%n",
+                pi, Math.PI, pi / Math.PI * 100);
     }
 
-    private static void printPi() {
-        int upperBound = 999999999;
-        int lowerBound = 3;
+    private static double getPi() {
+        final int upperBound = 999999999;
+        final int lowerBound = 3;
         double sum = 1;
-        double pi;
-        double percent;
-
         for (int i = lowerBound; i <= upperBound; i += 2) {
             if (i % 4 == 1) {
                 sum += (double) 1 / i;
                 continue;
             }
-
             if (i % 4 == 3) {
                 sum -= (double) 1 / i;
                 continue;
             }
-
             System.out.println("The machine runs out of control!");
         }
-
-        pi = 4 * sum;
-        percent = pi / Math.PI * 100;
-        System.out.println(
-                "π = "
-                        + pi
-                        + "\n"
-                        + "π of Math.PI = "
-                        + Math.PI
-                        + "\n"
-                        + "Percent of Math.PI = "
-                        + percent);
+        return 4 * sum;
     }
 }
