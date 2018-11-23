00fbd8ef69b1a3a13f60f8fc9f80baf59fa66bc0
Jεan Sacren <sakiwit@gmail.com>
2018-11-23 01:28:16 -0700

    Clean up not useful variable

--- src/main/java/org/dev/my/IntegerOfAnyRadixToDecimal.java
+++ src/main/java/org/dev/my/IntegerOfAnyRadixToDecimal.java
@@ -4,40 +4,29 @@ import java.util.Scanner;
 public class IntegerOfAnyRadixToDecimal {
     /** Prompt the user and convert to decimal. */
     public static void main(String[] args) {
-        String stringInteger;
-        String stringRadix;
-        int radix;
-
         Scanner inStream = new Scanner(System.in);
-
+        String stringInteger;
+        int radix;
         for ( ; ; ) {
             System.out.print("Enter a radix: ");
-            stringRadix = inStream.next();
-
-            if (isRadixValid(stringRadix)) {
-                radix = Integer.parseInt(stringRadix);
+            radix = inStream.nextInt();
+            if (isRadixValid(radix)) {
                 break;
             }
-
-            System.out.printf("%s is invalid. Try again.%n", stringRadix);
+            System.out.printf("%d is invalid. Try again.%n", radix);
         }
-
         System.out.print("Enter an integer: ");
         stringInteger = inStream.next();
         System.out.printf(
                 "%s is %d in decimal%n",
                 stringInteger, Integer.parseInt(stringInteger, radix));
-
         inStream.close();
     }
 
-    private static boolean isRadixValid(String s) {
-        final int radix = Integer.parseInt(s);
-
-        if (radix >= 2 && radix <= 16) {
+    private static boolean isRadixValid(int number) {
+        if (number >= 2 && number <= 16) {
             return true;
         }
-
         return false;
     }
 }
