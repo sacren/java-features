c4175acb539a7609c22b7ffca76c0f29a6176370
Jεan Sacren <sakiwit@gmail.com>
2018-11-25 03:51:17 -0700

    Add visual presentation of operation

--- src/main/java/org/dev/my/BitwiseOp.java
+++ src/main/java/org/dev/my/BitwiseOp.java
@@ -8,8 +8,17 @@ public class BitwiseOp {
         System.out.printf(
                 "0xAAAA555 is %d%n"
                         + "0x55551111 is %d%n"
+                        + "0xAAAA5555 & 0x55551111 = %12$S%n"
+                        + "0xAAAA5555 = %15$s%n"
+                        + "0x55551111 = %16$s%n"
                         + "0xAAAA5555 & 0x55551111 = %9$s%n"
+                        + "0xAAAA5555 | 0x55551111 = %13$S%n"
+                        + "0xAAAA5555 = %15$s%n"
+                        + "0x55551111 = %16$s%n"
                         + "0xAAAA5555 | 0x55551111 = %10$s%n"
+                        + "0xAAAA5555 ^ 0x55551111 = %14$S%n"
+                        + "0xAAAA5555 = %15$S%n"
+                        + "0x55551111 = %16$S%n"
                         + "0xAAAA5555 ^ 0x55551111 = %11$s%n"
                         + "MAX_VALUE in hex  is %6$S%n"
                         + "MAX_VALUE in decimal is %5$d%n"
@@ -23,6 +32,11 @@ public class BitwiseOp {
                 Integer.toBinaryString(z),
                 Integer.toBinaryString(x & y),
                 Integer.toBinaryString(x | y),
-                Integer.toBinaryString(x ^ y));
+                Integer.toBinaryString(x ^ y),
+                Integer.toHexString(x & y),
+                Integer.toHexString(x | y),
+                Integer.toHexString(x ^ y),
+                Integer.toBinaryString(x),
+                Integer.toBinaryString(y));
     }
 }
