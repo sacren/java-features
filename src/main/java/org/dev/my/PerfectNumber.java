1c8646c6ed9e3ee7740ef66d2abe1f8fd1811403
Jεan Sacren <sakiwit@gmail.com>
2018-11-29 03:38:06 -0700

    Add PerfectNumber class

---
+++ src/main/java/org/dev/my/PerfectNumber.java
@@ -0,0 +1,42 @@
+/**
+ * Prompt the user for an upper bound (a positive integer) and list all the
+ * perfect numbers less than or equal to this upper bound. It shall also list
+ * all the numbers that are neither deficient nor perfect.
+ */
+import java.util.Scanner;
+
+public class PerfectNumber {
+    /** Prompt for a positive integer and check if it is a perfect number. */
+    public static void main(String[] args) {
+        Scanner inputStream = new Scanner(System.in);
+        int number;
+        for ( ; ; ) {
+            System.out.print("Enter a positive integer: ");
+            number = inputStream.nextInt();
+            if (number > 0) {
+                break;
+            }
+            System.out.printf("%d is invalid. Try again.%n", number);
+        }
+        System.out.printf(
+                "%d is%s a perfect number%n",
+                number, isPerfect(number) ? "" : " not");
+        inputStream.close();
+    }
+
+    private static boolean isPerfect(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (i > number / 2) {
+                break;
+            }
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == number) {
+            return true;
+        }
+        return false;
+    }
+}
