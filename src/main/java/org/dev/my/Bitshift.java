905fb90c5d0db638e2142ec194e2d44edf948451
Jεan Sacren <sakiwit@gmail.com>
2018-11-29 02:50:02 -0700

    Add different right-shift operation

--- src/main/java/org/dev/my/Bitshift.java
+++ src/main/java/org/dev/my/Bitshift.java
@@ -11,12 +11,32 @@ public class Bitshift {
         System.out.printf("0xAAAA5555 << 1  = %X%n", x << 1);
         System.out.printf("0xAAAA5555       = %s%n", Integer.toBinaryString(x));
         System.out.printf("0xAAAA5555 << 1  =  %s%n", Integer.toBinaryString(x << 1));
+        System.out.printf("0x7FFFFFFF       = %d%n", Integer.MAX_VALUE);
         System.out.printf("0x7FFFFFFF       =  %s%n", Integer.toBinaryString(Integer.MAX_VALUE));
         System.out.printf("0x7FFFFFFF << 1  = %s%n",
                                                 Integer.toBinaryString(Integer.MAX_VALUE << 1));
+        System.out.printf("0x7FFFFFFF << 2  = %s%n",
+                                                Integer.toBinaryString(Integer.MAX_VALUE << 2));
+        System.out.printf(
+                "0x7FFFFFFF << 31        = %s%n",
+                Integer.toBinaryString(Integer.MAX_VALUE << 31));
+        System.out.printf(
+                "0x7FFFFFFF << 31 | 0x01 = %s%n",
+                Integer.toBinaryString(Integer.MAX_VALUE << 31 | 0x01));
+        System.out.printf("0x7FFFFFFF << 31 = %X%n", Integer.MAX_VALUE << 31);
+        System.out.printf("0x7FFFFFFF << 1  = %d%n", Integer.MAX_VALUE << 1);
+        System.out.printf("0x7FFFFFFF << 2  = %d%n", Integer.MAX_VALUE << 2);
+        System.out.printf(
+                "0x7FFFFFFF << 31        = %d%n",
+                Integer.MAX_VALUE << 31);
+        System.out.printf(
+                "0x7FFFFFFF << 31 | 0x01 = %d%n",
+                Integer.MAX_VALUE << 31 | 0x01);
+        System.out.printf("0x80000000       = %d%n", Integer.MIN_VALUE);
         System.out.printf("0x80000000       = %s%n", Integer.toBinaryString(Integer.MIN_VALUE));
         System.out.printf("0x80000000 << 1  = %s%n",
                                                 Integer.toBinaryString(Integer.MIN_VALUE << 1));
+        System.out.printf("0x80000000 << 1  = %d%n", Integer.MIN_VALUE << 1);
         System.out.printf("0xAAAA5555       = %s%n", Integer.toBinaryString(x));
         System.out.printf("0xAAAA5555 >> 1  = %s%n", Integer.toBinaryString(x >> 1));
         System.out.printf("0xAAAA5555 >> 1  = %d%n", x >> 1);
