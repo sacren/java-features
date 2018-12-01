de2cd8317a0f6c14db3e2f7a6784e9888f0d361d
Jεan Sacren <sakiwit@gmail.com>
2018-12-01 04:58:48 -0700

    Add countPrime() method
    Use array to manage the list of prime numbers rather than through the
    string.

--- src/main/java/org/dev/my/PrimeNumber.java
+++ src/main/java/org/dev/my/PrimeNumber.java
@@ -2,6 +2,7 @@
  * Prompt the user for an upper bound (a positive integer) and list all the
  * prime numbers less than or equal to this upper bound.
  */
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrimeNumber {
@@ -17,15 +18,19 @@ public class PrimeNumber {
             }
             System.out.printf("%d is invalid. Try again.%n", number);
         }
-        int count = 0;
-        System.out.println("List of prime numbers:");
+        final int count = countPrime(number);
+        int[] primeArray = new int[count];
+        int index = 0;
         for (int i = 2; i <= number; i++) {
             if (isPrime(i)) {
-                System.out.printf("%d%n", i);
-                count++;
+                primeArray[index++] = i;
             }
         }
-        System.out.printf("%d prime numbers (%.2f%%)%n%n", count, (double) count / number * 100);
+        System.out.printf(
+                "List of prime numbers:%n"
+                        + "%s%n"
+                        + "%d prime numbers (%.2f%%)%n%n",
+                Arrays.toString(primeArray), count, (double) count / number * 100);
         inputStream.close();
     }
 
@@ -36,5 +41,15 @@ public class PrimeNumber {
             }
         }
         return true;
+    }
+
+    private static int countPrime(int upperBound) {
+        int count = 0;
+        for (int i = 2; i <= upperBound; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
