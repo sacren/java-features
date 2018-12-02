d1014aebea915470a68b198ccfe4e5410d1c4a5a
Jεan Sacren <sakiwit@gmail.com>
2018-12-02 01:14:19 -0700

    Use array for list of perfect numbers

--- src/main/java/org/dev/my/PerfectNumber.java
+++ src/main/java/org/dev/my/PerfectNumber.java
@@ -3,6 +3,7 @@
  * perfect numbers less than or equal to this upper bound. It shall also list
  * all the numbers that are neither deficient nor perfect.
  */
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PerfectNumber {
@@ -18,29 +19,31 @@ public class PerfectNumber {
             }
             System.out.printf("%d is invalid. Try again.%n", number);
         }
-        String perfectNumbers = "";
-        String neither = "";
-        int perfectCount = 0;
-        int neitherCount = 0;
+
+        int[] countArray = new int[2]; /* [0]: perfect count, [1]: neither count */
         for (int i = 1; i <= number; i++) {
             if (isPerfect(i)) {
-                if (perfectNumbers.isEmpty()) {
-                    perfectNumbers += Integer.toString(i);
-                } else {
-                    perfectNumbers += " " + Integer.toString(i);
-                }
-                perfectCount++;
+                countArray[0]++;
                 continue;
             }
             if (isDeficient(i)) {
                 continue;
             }
-            if (neither.isEmpty()) {
-                neither += Integer.toString(i);
-            } else {
-                neither += " " + Integer.toString(i);
+            countArray[1]++;
+        }
+        int[] perfectArray = new int[countArray[0]];
+        int[] neitherArray = new int[countArray[1]];
+        int perfectCount = 0;
+        int neitherCount = 0;
+        for (int i = 1; i <= number; i++) {
+            if (isPerfect(i)) {
+                perfectArray[perfectCount++] = i;
+                continue;
             }
-            neitherCount++;
+            if (isDeficient(i)) {
+                continue;
+            }
+            neitherArray[neitherCount++] = i;
         }
         System.out.printf(
                 "List of perfect numbers:%n"
@@ -49,8 +52,8 @@ public class PerfectNumber {
                         + "List of numbers neither perfect nor deficient:%n"
                         + "%s%n"
                         + "%d neither perfect nor deficient (%.2f%%)%n",
-                perfectNumbers, perfectCount, (double) perfectCount / number * 100,
-                neither, neitherCount, (double) neitherCount / number * 100);
+                Arrays.toString(perfectArray), perfectCount, (double) perfectCount / number * 100,
+                Arrays.toString(neitherArray), neitherCount, (double) neitherCount / number * 100);
         inputStream.close();
     }
 
