27877608ec8376c0bd5897b46fbc5883e0a096e5
Jεan Sacren <sakiwit@gmail.com>
2018-12-05 02:20:04 -0700

    Fix class document

--- src/main/java/org/dev/my/PrimeNumber.java
+++ src/main/java/org/dev/my/PrimeNumber.java
@@ -3,7 +3,7 @@
  * prime numbers less than or equal to this upper bound.
  *
  * Display all the numbers that is equal to the product of all its prime
- * factors excluding 1 and the number itself.
+ * factors less than the number itself.
  */
 import java.util.Arrays;
 import java.util.Scanner;
