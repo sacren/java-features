402c9543805f571cb957934be0a3236213e47853
Jεan Sacren <sakiwit@gmail.com>
2018-12-13 02:59:56 -0700

    Clean up not useful method and excessive newlines

--- src/main/java/org/dev/my/OverFlowTest.java
+++ src/main/java/org/dev/my/OverFlowTest.java
@@ -1,50 +1,26 @@
 /** Overflow demo. */
 public class OverFlowTest {
+    /** Display various overflows. */
     public static void main(String[] args) {
-        printTestResult();
-    }
-
-    private static void printTestResult() {
-        int integerMax = Integer.MAX_VALUE;
-        int overFlowMax1 = integerMax + 1;
-        int overFlowMax2 = integerMax + 2;
-        int overFlowMax3 = integerMax * integerMax;
-        long longNumber1 = integerMax + 1L;
-        int integerMin = Integer.MIN_VALUE;
-        int overFlowMin1 = integerMin + 1;
-        int overFlowMin2 = integerMin + 2;
-        int overFlowMin3 = integerMin * integerMin;
-        long longNumber2 = integerMin - 1L;
-
-        System.out.println(
-                "int(max) = "
-                        + integerMax
-                        + "\n"
-                        + "int(max) + 1 = "
-                        + overFlowMax1
-                        + "\n"
-                        + "int(max) + 2 = "
-                        + overFlowMax2
-                        + "\n"
-                        + "int(max) * int(max) = "
-                        + overFlowMax3
-                        + "\n"
-                        + "int(max) + 1L = "
-                        + longNumber1
-                        + "\n\n"
-                        + "int(min) = "
-                        + integerMin
-                        + "\n"
-                        + "int(min) - 1 = "
-                        + overFlowMin1
-                        + "\n"
-                        + "int(min) - 2 = "
-                        + overFlowMin2
-                        + "\n"
-                        + "int(min) * int(min) = "
-                        + overFlowMin3
-                        + "\n"
-                        + "int(min) - 1L = "
-                        + longNumber2);
+        final int integerMax = Integer.MAX_VALUE;
+        final int overFlowMax1 = integerMax + 1;
+        final int overFlowMax2 = integerMax + 2;
+        final int overFlowMax3 = integerMax * integerMax;
+        final long longNumber1 = integerMax + 1L;
+        final int integerMin = Integer.MIN_VALUE;
+        final int overFlowMin1 = integerMin + 1;
+        final int overFlowMin2 = integerMin + 2;
+        final int overFlowMin3 = integerMin * integerMin;
+        final long longNumber2 = integerMin - 1L;
+        System.out.println("int(max) = " + integerMax);
+        System.out.println("int(max) + 1 = " + overFlowMax1);
+        System.out.println("int(max) + 2 = " + overFlowMax2);
+        System.out.println("int(max) * int(max) = " + overFlowMax3);
+        System.out.println("int(max) + 1L = " + longNumber1);
+        System.out.println("int(min) = " + integerMin);
+        System.out.println("int(min) - 1 = " + overFlowMin1);
+        System.out.println("int(min) - 2 = " + overFlowMin2);
+        System.out.println("int(min) * int(min) = " + overFlowMin3);
+        System.out.println("int(min) - 1L = " + longNumber2);
     }
 }
