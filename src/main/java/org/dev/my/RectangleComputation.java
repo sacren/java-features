dab6ed3ff296c201b0d76f13ee9add3951202437
Jεan Sacren <sakiwit@gmail.com>
2018-12-14 04:17:16 -0700

    Use Rectangle object for area and perimeter calculation

--- src/main/java/org/dev/my/RectangleComputation.java
+++ src/main/java/org/dev/my/RectangleComputation.java
@@ -1,15 +1,10 @@
 /** Print the area and perimeter of a rectangle. */
 public class RectangleComputation {
     public static void main(String[] args) {
-        final double length = 3.4;
-        final double width = 2.0;
-        double area;
-        double perimeter;
-
-        area = length * width;
-        perimeter = (length + width) * 2;
-
-        System.out.println("The rectangle area is " + area);
-        System.out.println("The rectangle perimeter is " + perimeter);
+        final float length = 3.4f;
+        final float width = 2.0f;
+        Rectangle rectangleInstance = new Rectangle(length, width);
+        System.out.println("The rectangle area is " + rectangleInstance.getArea());
+        System.out.println("The rectangle perimeter is " + rectangleInstance.getPerimeter());
     }
 }
