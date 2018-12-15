aedb94da0a47ca7e786f59e2b211dfd3da0339d1
Jεan Sacren <sakiwit@gmail.com>
2018-12-15 02:53:40 -0700

    Add input from the user

--- src/main/java/org/dev/my/RectangleComputation.java
+++ src/main/java/org/dev/my/RectangleComputation.java
@@ -1,8 +1,28 @@
 /** Print the area and perimeter of a rectangle. */
+import java.util.Scanner;
+
 public class RectangleComputation {
     public static void main(String[] args) {
-        final float length = 3.4f;
-        final float width = 2.0f;
+        Scanner inputStream = new Scanner(System.in);
+        float length;
+        for ( ; ; ) {
+            System.out.print("Enter rectangle length: ");
+            length = inputStream.nextFloat();
+            if (length >= 0) {
+                break;
+            }
+            System.out.printf("%f is invalid. Try again.%n", length);
+        }
+        float width;
+        for ( ; ; ) {
+            System.out.print("Enter rectangle width: ");
+            width = inputStream.nextFloat();
+            if (width >= 0) {
+                break;
+            }
+            System.out.printf("%f is invalid. Try again.%n", width);
+        }
+        inputStream.close();
         Rectangle rectangleInstance = new Rectangle(length, width);
         System.out.printf(
                 "The rectangle area is %.2f%n"
