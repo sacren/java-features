728f1de906eff198b7b96f62d0c2f1db08257ae8
Jεan Sacren <sakiwit@gmail.com>
2018-12-24 02:40:31 -0700

    Add Time class

---
+++ src/main/java/org/dev/my/Time.java
@@ -0,0 +1,104 @@
+/**
+ * Time class.
+ *
+ * <p>Private data with default values:
+ * -second:int = 0
+ * -minute:int = 0
+ * -hour:int = 0
+ *
+ * <p>Constructor:
+ * +Time()
+ * +Time(second:int, minute:int, hour:int)
+ *
+ * <p>Public methods:
+ * +getSecond():int
+ * +setSecond(second:int):void
+ * +getMinute():int
+ * +setMinute(minute:int):void
+ * +getHour():int
+ * +setHour(hour:int):void
+ * +setTime(second:int, minute:int, hour:int):void
+ * +nextSecond():Time
+ * +toString():String
+ */
+public class Time {
+    /* public static data */
+    public static final int DEFAULT_SECOND = 0;
+    public static final int DEFAULT_MINUTE = 0;
+    public static final int DEFAULT_HOUR = 0;
+
+    /* private instance data */
+    private int second;
+    private int minute;
+    private int hour;
+
+    /** Default constructor. */
+    public Time() {
+        this.second = DEFAULT_SECOND;
+        this.minute = DEFAULT_MINUTE;
+        this.hour = DEFAULT_HOUR;
+    }
+
+    /** Custom constructor. */
+    public Time(int second, int minute, int hour) {
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+    }
+
+    /** Public accessor for second. */
+    public int getSecond() {
+        return second;
+    }
+
+    /** Public mutator for second. */
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    /** Public accessor for minute. */
+    public int getMinute() {
+        return minute;
+    }
+
+    /** Public mutator for minute. */
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    /** Public accessor for hour. */
+    public int getHour() {
+        return hour;
+    }
+
+    /** Public mutator for hour. */
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    /** Public method to set time. */
+    public void setTime(int second, int minute, int hour) {
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+    }
+
+    /** Public method to advance time by one second. */
+    public Time nextSecond() {
+        if (++second == 60) {
+            second = 0;
+            if (++minute == 60) {
+                minute = 0;
+                if (++hour == 24) {
+                    hour = 0;
+                }
+            }
+        }
+        return this;
+    }
+
+    /** Time description. */
+    public String toString() {
+        return String.format("[%02d:%02d:%02d]%n", hour, minute, second);
+    }
+}
---
+++ src/main/java/org/dev/my/TimeDriver.java
@@ -0,0 +1,47 @@
+/**
+ * Driver for Time.
+ */
+import java.util.Scanner;
+
+public class TimeDriver {
+    /** Prompt the user for time input and initialize Time. */
+    public static void main(String[] args) {
+        Scanner inStream = new Scanner(System.in);
+        int hour;
+        for ( ; ; ) {
+            System.out.print("Enter the hour: ");
+            hour = inStream.nextInt();
+            if (hour >= 0 && hour <= 24) {
+                break;
+            }
+            System.out.printf("%d is invalid. Try again.%n", hour);
+        }
+        int minute;
+        for ( ; ; ) {
+            System.out.print("Enter the minute: ");
+            minute = inStream.nextInt();
+            if (minute >= 0 && minute <= 60) {
+                break;
+            }
+            System.out.printf("%d is invalid. Try again.%n", minute);
+        }
+        int second;
+        for ( ; ; ) {
+            System.out.print("Enter the second: ");
+            second = inStream.nextInt();
+            if (second >= 0 && second <= 60) {
+                break;
+            }
+            System.out.printf("%d is invalid. Try again.%n", second);
+        }
+        inStream.close();
+        Time currentTime = new Time(second, minute, hour);
+        System.out.println();
+        System.out.print(currentTime);
+        System.out.printf(
+                "Current time is %02d:%02d:%02d%n",
+                currentTime.getHour(),
+                currentTime.getMinute(),
+                currentTime.getSecond());
+    }
+}
