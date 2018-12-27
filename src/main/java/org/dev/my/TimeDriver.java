5759115737b1b15af62f60cb77675fc5272cf23d
Jεan Sacren <sakiwit@gmail.com>
2018-12-27 01:29:26 -0700

    Remove useless check for input

--- src/main/java/org/dev/my/TimeDriver.java
+++ src/main/java/org/dev/my/TimeDriver.java
@@ -7,33 +7,12 @@ public class TimeDriver {
     /** Prompt the user for time input and initialize Time. */
     public static void main(String[] args) {
         Scanner inStream = new Scanner(System.in);
-        int hour;
-        for ( ; ; ) {
-            System.out.print("Enter the hour: ");
-            hour = inStream.nextInt();
-            if (hour >= 0 && hour <= 24) {
-                break;
-            }
-            System.out.printf("%d is invalid. Try again.%n", hour);
-        }
-        int minute;
-        for ( ; ; ) {
-            System.out.print("Enter the minute: ");
-            minute = inStream.nextInt();
-            if (minute >= 0 && minute <= 60) {
-                break;
-            }
-            System.out.printf("%d is invalid. Try again.%n", minute);
-        }
-        int second;
-        for ( ; ; ) {
-            System.out.print("Enter the second: ");
-            second = inStream.nextInt();
-            if (second >= 0 && second <= 60) {
-                break;
-            }
-            System.out.printf("%d is invalid. Try again.%n", second);
-        }
+        System.out.print("Enter the hour: ");
+        final int hour = inStream.nextInt();
+        System.out.print("Enter the minute: ");
+        final int minute = inStream.nextInt();
+        System.out.print("Enter the second: ");
+        final int second = inStream.nextInt();
         inStream.close();
         Time currentTime = new Time(second, minute, hour);
         System.out.println();
