4a880556a4fb124b0a51f46ac6d847c8959a7eed
Jεan Sacren <sakiwit@gmail.com>
2018-12-29 02:18:45 -0700

    Add the use of setter

--- src/main/java/org/dev/my/AuthorDriver.java
+++ src/main/java/org/dev/my/AuthorDriver.java
@@ -14,5 +14,13 @@ public class AuthorDriver {
                 "The C Programming Language is authored by %s.%n"
                         + "Send email to %s for questions.%n",
                 author.getName(), author.getEmail());
+        System.out.println();
+        System.out.printf("After deleting email address.%n%n");
+        author.setEmail("");
+        System.out.print(author);
+        System.out.println();
+        System.out.printf("After restoring email address.%n%n");
+        author.setEmail(email);
+        System.out.print(author);
     }
 }
