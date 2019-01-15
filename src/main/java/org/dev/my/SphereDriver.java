06a579e6239ecfba19ce7d535884f2123d03e8de
Jεan Sacren <sakiwit@gmail.com>
2019-01-15 03:45:51 -0700

    Clean up not useful static method of SphereDriver

--- src/main/java/org/dev/my/SphereDriver.java
+++ src/main/java/org/dev/my/SphereDriver.java
@@ -7,13 +7,11 @@ public class SphereDriver {
     /** Prompt the user for the radius and initialize object. */
     public static void main(String[] args) {
         Scanner inStream = new Scanner(System.in);
+        double radius;
         System.out.print("Enter the sphere radius: ");
-        double radius = inStream.nextDouble();
+        radius = inStream.nextDouble();
         inStream.close();
-        getSphere(radius);
-    }
 
-    private static void getSphere(double radius) {
         Sphere sphere = new Sphere(radius);
         System.out.println();
         System.out.print(sphere);
@@ -21,6 +19,6 @@ public class SphereDriver {
                 "Sphere radius is %.2f%n"
                         + "Sphere surface area is %.2f%n"
                         + "Sphere volume is %.2f%n",
-                        sphere.getRadius(), sphere.getArea(), sphere.getVolume());
+                sphere.getRadius(), sphere.getArea(), sphere.getVolume());
     }
 }
