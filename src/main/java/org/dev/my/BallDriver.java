9095b5803681383c85ae316df8be71c159d5da7d
Jεan Sacren <sakiwit@gmail.com>
2019-01-17 02:37:14 -0700

    Add tests of Ball getter and setter

--- src/main/java/org/dev/my/BallDriver.java
+++ src/main/java/org/dev/my/BallDriver.java
@@ -2,8 +2,34 @@
  * Ball driver.
  */
 public class BallDriver {
+    /** Instantiate Ball and apply methods. */
     public static void main(String[] args) {
         Ball ball = new Ball(1, 2, 11, 12);
         System.out.print(ball);
+        System.out.printf(
+                "axisX is %.2f%n"
+                        + "axisY is %.2f%n"
+                        + "axisXStep is %.2f%n"
+                        + "axisYStep is %.2f%n",
+                ball.getX(),
+                ball.getY(),
+                ball.getXStep(),
+                ball.getYStep());
+        System.out.println();
+        System.out.printf("After doubling all the data.%n%n");
+        ball.setX(ball.getX() * 2);
+        ball.setY(ball.getY() * 2);
+        ball.setXStep(ball.getXStep() * 2);
+        ball.setYStep(ball.getYStep() * 2);
+        System.out.print(ball);
+        System.out.printf(
+                "axisX is %.2f%n"
+                        + "axisY is %.2f%n"
+                        + "axisXStep is %.2f%n"
+                        + "axisYStep is %.2f%n",
+                ball.getX(),
+                ball.getY(),
+                ball.getXStep(),
+                ball.getYStep());
     }
 }
