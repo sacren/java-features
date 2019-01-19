f04633e011bf989225583b41d9ec61344a31f0c6
Jεan Sacren <sakiwit@gmail.com>
2019-01-19 02:49:50 -0700

    Clean up not useful variable declaration

--- src/main/java/org/dev/my/StudentDriver.java
+++ src/main/java/org/dev/my/StudentDriver.java
@@ -4,9 +4,7 @@
 public class StudentDriver {
     /** Instantiate Student and display student info. */
     public static void main(String[] args) {
-        final String name = "Charlie Smith";
-        final String address = "24 College Drive, Anytown, Anystate";
-        Student student = new Student(name, address);
+        Student student = new Student("Charlie Smith", "24 College Drive, Anytown, Anystate");
         System.out.print(student);
     }
 }
