95ac21270996a5c83937bafa150d8f144df9d1af
Jεan Sacren <sakiwit@gmail.com>
2019-01-19 03:59:55 -0700

    Add tests of Student getter and setter

--- src/main/java/org/dev/my/StudentDriver.java
+++ src/main/java/org/dev/my/StudentDriver.java
@@ -6,5 +6,19 @@ public class StudentDriver {
     public static void main(String[] args) {
         Student student = new Student("Charlie Smith", "24 College Drive, Anytown, Anystate");
         System.out.print(student);
+        System.out.printf(
+                "Name is %s%n"
+                        + "Address is %s%n",
+                student.getName(),
+                student.getAddress());
+        System.out.println();
+        System.out.printf("After updating the address.%n%n");
+        student.setAddress("46 University Ave., Anytown, Anystate");
+        System.out.print(student);
+        System.out.printf(
+                "Name is %s%n"
+                        + "Address is %s%n",
+                student.getName(),
+                student.getAddress());
     }
 }
