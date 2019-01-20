2e7a5f02d2481e00c9c3613d4713587ac1225ea2
Jεan Sacren <sakiwit@gmail.com>
2019-01-20 02:43:37 -0700

    Check the first character rather than the last of the string

--- src/main/java/org/dev/my/SumDigits.java
+++ src/main/java/org/dev/my/SumDigits.java
@@ -10,17 +10,18 @@ public class SumDigits {
         if (Integer.parseInt(args[0]) < 0) {
             throw new IllegalArgumentException("Argument can't be negative number!");
         }
-        String digitString = "";
-        int sum = 0;
-        int index = 0;
         char[] charArray = args[0].toCharArray();
+        int sum;
+        sum = Character.digit(charArray[0], 10);
+        String digitString;
+        digitString = Character.toString(charArray[0]);
+        int index = 0;
         for (char c: charArray) {
-            sum += Character.digit(c, 10);
-            if (index++ == charArray.length - 1) {
-                digitString += Character.toString(c);
-                break;
+            if (index++ == 0) {
+                continue;
             }
-            digitString += Character.toString(c) + " + ";
+            sum += Character.digit(c, 10);
+            digitString += " + " + Character.toString(c);
         }
         System.out.printf("Sum of digits: %s = %d%n", digitString, sum);
     }
