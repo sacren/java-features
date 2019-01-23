8a8c65cf1c8e341e7a5499a27b53ef14bc4eb38c
Jεan Sacren <sakiwit@gmail.com>
2019-01-23 02:38:08 -0700

    Clean up redundant calls of print()

--- src/main/java/org/dev/my/AuthorDriver.java
+++ src/main/java/org/dev/my/AuthorDriver.java
@@ -8,8 +8,7 @@ public class AuthorDriver {
         String email = "dmr@bell-labs.com";
         final char gender = 'M';
         Author author = new Author(name, email, gender);
-        System.out.print(author);
-        System.out.println();
+        System.out.println(author);
         System.out.printf(
                 "The C Programming Language is authored by %s.%n"
                         + "Send email to %s for questions.%n",
@@ -17,8 +16,7 @@ public class AuthorDriver {
         System.out.println();
         System.out.printf("After deleting email address.%n%n");
         author.setEmail("");
-        System.out.print(author);
-        System.out.println();
+        System.out.println(author);
         System.out.printf("After restoring email address.%n%n");
         author.setEmail(email);
         System.out.print(author);
