0325126aeaddfbe62b49a4306d48b1008d487e0a
Jεan Sacren <sakiwit@gmail.com>
2019-01-25 03:57:01 -0700

    Add color to Cylinder instance string

--- src/main/java/org/dev/my/Cylinder.java
+++ src/main/java/org/dev/my/Cylinder.java
@@ -76,7 +76,7 @@ public class Cylinder extends Circle {
     /** Cylinder instance description. */
     public String toString() {
         return String.format(
-                "[Cylinder (Radius=%.2f, Height=%.2f)]%n",
-                getRadius(), height);
+                "[Cylinder (Radius=%.2f, Height=%.2f, Color=%s)]%n",
+                getRadius(), height, getColor());
     }
 }
