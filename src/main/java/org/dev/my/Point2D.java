357ea9542d0be2822f10bd6670df8a5efe8a7212
Jεan Sacren <sakiwit@gmail.com>
2019-01-26 03:33:58 -0700

    Add Point2D class

---
+++ src/main/java/org/dev/my/Point2D.java
@@ -0,0 +1,64 @@
+/**
+ * Point2D: superclass over Point3D.
+ *
+ * <p>Private data with default values:
+ * -axisX:int = 0
+ * -axisY:int = 0
+ *
+ * <p>Constructor:
+ * +Point()
+ * +Point(axisX:int, axisY:int)
+ *
+ * <p>Public methods:
+ * +getX():int
+ * +setX(axisX:int):void
+ * +getY():int
+ * +setY(axisY:int):void
+ * +toString():string
+ */
+public class Point2D {
+    /* public static data */
+    public static final int DEFAULT_X = 0;
+    public static final int DEFAULT_Y = 0;
+
+    /* private instance data */
+    private int axisX;
+    private int axisY;
+
+    /* default constructor */
+    public Point2D() {
+        axisX = DEFAULT_X;
+        axisY = DEFAULT_Y;
+    }
+
+    /* custom constructor */
+    public Point2D(int axisX, int axisY) {
+        this.axisX = axisX;
+        this.axisY = axisY;
+    }
+
+    /** getter of axisX. */
+    public int getX() {
+        return axisX;
+    }
+
+    /** setter of axisX. */
+    public void setX(int axisX) {
+        this.axisX = axisX;
+    }
+
+    /** getter of axisY. */
+    public int getY() {
+        return axisY;
+    }
+
+    /** setter of axisY. */
+    public void setY(int axisY) {
+        this.axisY = axisY;
+    }
+
+    /** Point2D description. */
+    public String toString() {
+        return String.format("(%d, %d)", axisX, axisY);
+    }
+}
