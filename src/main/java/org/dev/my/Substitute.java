2a07271a70c9660ad83195c8699864d280bef565
Jεan Sacren <sakiwit@gmail.com>
2019-02-03 02:41:14 -0700

    Put comment in the right place

--- src/main/java/org/dev/my/Substitute.java
+++ src/main/java/org/dev/my/Substitute.java
@@ -12,8 +12,8 @@ public class Substitute {
     public static void main(String[] args) {
         double radius = 1;
         double height = 2;
-        /* Subclass instance is converted to that of superclass */
         Circle circle = new Circle(radius);
+        /* Subclass instance is converted to that of superclass */
         Circle substituteCircle = new Cylinder(radius, height);
         System.out.printf(
                 "The circle radius is %.2f%n"
