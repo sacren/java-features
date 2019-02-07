41c6840cc3439439f293cae8985b95832f557de0
Jεan Sacren <sakiwit@gmail.com>
2019-02-07 02:52:34 -0700

    Add formatted display of leap years

--- src/main/java/org/dev/my/LeapYears.java
+++ src/main/java/org/dev/my/LeapYears.java
@@ -1,21 +1,27 @@
-/** All the leap years between AD1 and AD2010, and also print the number of leap years. */
+/**
+ * All the leap years between AD1 and AD2010 and the number of those leap
+ * years.
+ */
 public class LeapYears {
+    /** Find all the leap years. */
     public static void main(String[] args) {
-        printLeapYears();
-    }
-
-    private static void printLeapYears() {
-        int upperBound = 2010;
-        int lowerBound = 1;
-        int count = 0;
-
-        for (int i = lowerBound; i <= upperBound; i++) {
-            if (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)) {
-                System.out.println(i);
-                count++;
+        final int begin = 1;
+        final int end = 2010;
+        int total = 0;
+        int count = 1;
+        System.out.println();
+        System.out.printf("List of leap years between AD1 and AD2010:%n%n");
+        for (int year = begin; year <= end; year++) {
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.printf("%-5d", year);
+                if (count++ % 20 == 0) {
+                    System.out.println();
+                }
+                total++;
             }
         }
-
-        System.out.println("Number of leap years is " + count + ".");
+        System.out.println();
+        System.out.println();
+        System.out.printf("Number of leap years is %d%n", total);
     }
 }
