8f57b4ee3b64b9596707e6b071d361379e54bc56
Jεan Sacren <sakiwit@gmail.com>
2019-02-17 01:49:46 -0700

    Add CastTop superclass

---
+++ src/main/java/org/dev/my/CastTop.java
@@ -0,0 +1,18 @@
+/**
+ * CastTop superclass.
+ *
+ * <p>Constructor:
+ * +CastTop()
+ *
+ * <p>Public methods:
+ * +toString():String
+ */
+public class CastTop {
+    /** CastTop default constructor. */
+    public CastTop() { }
+
+    /** CastTop description. */
+    public String toString() {
+        return "This is Top superclass.";
+    }
+}
---
+++ src/main/java/org/dev/my/TestCast.java
@@ -0,0 +1,10 @@
+/**
+ * Test driver for CastTop, CastMiddle and CastBottom.
+ */
+public class TestCast {
+    /** Test driver for CastTop, CastMiddle and CastBottom. */
+    public static void main(String[] args) {
+        CastTop top = new CastTop();
+        System.out.println(top);
+    }
+}
