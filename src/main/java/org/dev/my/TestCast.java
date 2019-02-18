0a3fa177bdb8d5a426fc3ef65bff3e27bb607daf
Jεan Sacren <sakiwit@gmail.com>
2019-02-18 01:57:43 -0700

    Add comments for upcast and downcast

--- src/main/java/org/dev/my/TestCast.java
+++ src/main/java/org/dev/my/TestCast.java
@@ -4,12 +4,16 @@
 public class TestCast {
     /** Test driver for CastTop, CastMiddle and CastBottom. */
     public static void main(String[] args) {
+        /* no cast */
         CastTop top = new CastTop();
         System.out.println(top);
+        /* upcast */
         CastTop middle = new CastMiddle();
         System.out.println(middle);
+        /* downcast */
         CastMiddle downCast = (CastMiddle)middle;
         System.out.println(downCast);
+        /* upcast */
         CastMiddle bottom = new CastBottom();
         System.out.println(bottom);
     }
