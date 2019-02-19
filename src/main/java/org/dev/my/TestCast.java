cadca82cb55ccafd63a7f90f437dc4acea74f54e
Jεan Sacren <sakiwit@gmail.com>
2019-02-19 04:56:10 -0700

    Update interactive messages

--- src/main/java/org/dev/my/TestCast.java
+++ src/main/java/org/dev/my/TestCast.java
@@ -5,27 +5,64 @@ public class TestCast {
     /** Test driver for CastTop, CastMiddle and CastBottom. */
     public static void main(String[] args) {
         /* no cast */
+        System.out.println();
+        System.out.println("=== No cast ===");
+        System.out.println();
         CastTop top = new CastTop();
         System.out.println(top);
         /* upcast */
+        System.out.println();
+        System.out.println("=== Upcast ===");
+        System.out.println();
         CastTop middle = new CastMiddle();
         System.out.println(middle);
-        /* downcast */
-        System.out.println((CastMiddle)middle);
-        /* upcast */
         CastMiddle bottom = new CastBottom();
         System.out.println(bottom);
         System.out.println((CastTop)bottom);
+        /* downcast */
+        System.out.println();
+        System.out.println("=== Downcast ===");
+        System.out.println();
+        System.out.println((CastMiddle)middle);
         /* double upcast */
+        System.out.println();
+        System.out.println("=== Double upcast ===");
+        System.out.println();
         CastTop anotherTop = new CastBottom();
         System.out.println(anotherTop);
         /* double downcast */
+        System.out.println();
+        System.out.println("=== Double downcast ===");
+        System.out.println();
         System.out.println((CastBottom)anotherTop);
         /* instanceof operator */
-        System.out.println(top instanceof CastTop);
-        System.out.println(middle instanceof CastTop);
-        System.out.println(bottom instanceof CastTop);
-        System.out.println(top instanceof CastMiddle);
-        System.out.println(middle instanceof CastBottom);
+        System.out.println();
+        System.out.println("=== instanceof operator ===");
+        System.out.println();
+        if (top instanceof CastTop) {
+            System.out.println("top is an instance of CastTop class.");
+        } else {
+            System.out.println("top is not an instance of CastTop class.");
+        }
+        if (middle instanceof CastTop) {
+            System.out.println("middle is an instance of CastTop class.");
+        } else {
+            System.out.println("middle is not an instance of CastTop class.");
+        }
+        if (bottom instanceof CastTop) {
+            System.out.println("bottom is an instance of CastTop class.");
+        } else {
+            System.out.println("bottom is not an instance of CastTop class.");
+        }
+        if (top instanceof CastMiddle) {
+            System.out.println("top is an instance of CastMiddle class.");
+        } else {
+            System.out.println("top is not an instance of CastMiddle class.");
+        }
+        if (middle instanceof CastBottom) {
+            System.out.println("middle is an instance of CastBottom class.");
+        } else {
+            System.out.println("middle is not an instance of CastBottom class.");
+        }
     }
 }
