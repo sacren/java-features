7d62f10aa798970f87e0495d45618a28606d2766
Jεan Sacren <sakiwit@gmail.com>
2019-02-20 02:30:08 -0700

    Use sensible name for FireMonster instance

--- src/main/java/org/dev/my/TestMonster.java
+++ src/main/java/org/dev/my/TestMonster.java
@@ -4,9 +4,9 @@
 public class TestMonster {
     /** Test driver for various monsters. */
     public static void main(String[] args) {
-        Monster monster = new FireMonster("f2a2");
-        System.out.println(monster);
-        System.out.println(monster.attack());
+        Monster fireMonster = new FireMonster("f2a2");
+        System.out.println(fireMonster);
+        System.out.println(fireMonster.attack());
         Monster waterMonster = new WaterMonster("w2a2");
         System.out.println();
         System.out.println(waterMonster);
