d4313582fce0a1fdbbe5e455733d6c6e9e0a86f8
Jεan Sacren <sakiwit@gmail.com>
2019-02-24 04:51:13 -0700

    Add Hydrogen subclass

---
+++ src/main/java/org/dev/my/Hydrogen.java
@@ -0,0 +1,25 @@
+/**
+ * Hydrogen subclass extends Atom superclass.
+ *
+ * <p>Constructor:
+ * +Hydrogen(atomicNumber:int)
+ *
+ * <p>Public methods:
+ * +toString():String
+ */
+public class Hydrogen extends Atom {
+    /* private static data */
+    private static final int HYDROGEN_ATOMIC_NUMBER = 1;
+
+    /** Hydrogen custom constructor. */
+    public Hydrogen() {
+        super(HYDROGEN_ATOMIC_NUMBER);
+    }
+
+    /** Hydrogen instance description. */
+    public String toString() {
+        return String.format(
+                "Hydrogen has atomic number %d",
+                getAtomicNumber());
+    }
+}
