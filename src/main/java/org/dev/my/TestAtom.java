a24997bc5d9c7e26ea1509bfb546567abb720e01
Jεan Sacren <sakiwit@gmail.com>
2019-02-25 02:02:59 -0700

    Add test driver for Atom superclass

---
+++ src/main/java/org/dev/my/TestAtom.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for Atom superclass.
+ */
+public class TestAtom {
+    /** Test driver for Atom instance. */
+    public static void main(String[] args) {
+        Hydrogen hydrogen = new Hydrogen();
+        System.out.println(hydrogen);
+        Carbon carbon = new Carbon();
+        System.out.println(carbon);
+    }
+}
