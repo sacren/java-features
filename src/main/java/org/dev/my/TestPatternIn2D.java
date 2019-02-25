cca73dd8d86ebf7839300895baa95109102fb2ed
Jεan Sacren <sakiwit@gmail.com>
2019-02-25 03:44:01 -0700

    Use formatted string rather than printout

--- src/main/java/org/dev/my/TestPatternIn2D.java
+++ src/main/java/org/dev/my/TestPatternIn2D.java
@@ -116,319 +116,306 @@
  *         (l)
  */
 public class TestPatternIn2D {
-    /** Entry point. */
+    /** Display various 2-D patterns. */
     public static void main(String[] args) {
-        triangleBottomLeft();
+        System.out.println();
+        System.out.println("List of various 2-D patterns.");
+        System.out.println();
+        System.out.print(triangleBottomLeft());
         System.out.printf("%9s%n%n", "(a)");
-        triangleTopLeft();
+        System.out.print(triangleTopLeft());
         System.out.printf("%9s%n%n", "(b)");
-        triangleTopRight();
+        System.out.print(triangleTopRight());
         System.out.printf("%9s%n%n", "(c)");
-        triangleBottomRight();
+        System.out.print(triangleBottomRight());
         System.out.printf("%9s%n%n", "(d)");
-        emptySquare();
+        System.out.print(emptySquare());
         System.out.printf("%8s%n%n", "(e)");
-        mirrorLetterZ();
+        System.out.print(mirrorLetterZ());
         System.out.printf("%8s%n%n", "(f)");
-        letterZ();
+        System.out.print(letterZ());
         System.out.printf("%8s%n%n", "(g)");
-        hourGlass();
+        System.out.print(hourGlass());
         System.out.printf("%8s%n%n", "(h)");
-        hourGlassInBox();
+        System.out.print(hourGlassInBox());
         System.out.printf("%8s%n%n", "(i)");
-        parallelogram();
+        System.out.print(parallelogram());
         System.out.printf("%9s%n%n", "(j)");
-        solidSquare();
+        System.out.print(solidSquare());
         System.out.printf("%9s%n%n", "(k)");
-        checkerBoard();
+        System.out.print(checkerBoard());
         System.out.printf("%8s%n", "(l)");
     }
 
-    /* Pattern of square with empty enter. */
-    private static void emptySquare() {
+    /* Pattern of square with empty center. */
+    private static String emptySquare() {
         final int size = 7;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (row % size > 1 && col % size > 1) {
-                    System.out.print("  ");
+                    s += String.format("  ");
                     continue;
                 }
-
                 if (col == size) {
-                    System.out.println('#');
+                    s += String.format("#%n");
                     continue;
                 }
-
-                System.out.print("# ");
+                s += String.format("# ");
             }
         }
+        return s;
     }
 
     /* Pattern of triangle with right angle at bottom left. */
-    private static void triangleBottomLeft() {
+    private static String triangleBottomLeft() {
         final int size = 8;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (col == row) {
-                    System.out.println("#");
+                    s += String.format("#%n");
                     break;
                 }
-
-                System.out.print("# ");
+                s += String.format("# ");
             }
         }
+        return s;
     }
 
     /* Pattern of triangle with right angle at top left. */
-    private static void triangleTopLeft() {
+    private static String triangleTopLeft() {
         final int size = 8;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (col + row > size) {
-                    System.out.println("#");
+                    s += String.format("#%n");
                     break;
                 }
-
-                System.out.print("# ");
+                s += String.format("# ");
             }
         }
+        return s;
     }
 
     /* Pattern of triangle with right angle at top right. */
-    private static void triangleTopRight() {
+    private static String triangleTopRight() {
         final int size = 8;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (col == size) {
-                    System.out.println("#");
+                    s += String.format("#%n");
                     break;
                 }
-
                 if (row > col) {
-                    System.out.print("  ");
+                    s += String.format("  ");
                     continue;
                 }
-
-                System.out.print("# ");
+                s += String.format("# ");
             }
         }
+        return s;
     }
 
     /* Pattern of triangle with right angle at bottom right. */
-    private static void triangleBottomRight() {
+    private static String triangleBottomRight() {
         final int size = 8;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (col == size) {
-                    System.out.println("#");
+                    s += String.format("#%n");
                     break;
                 }
-
                 if (col + row <= size) {
-                    System.out.print("  ");
+                    s += String.format("  ");
                     continue;
                 }
-
-                System.out.print("# ");
+                s += String.format("# ");
             }
         }
+        return s;
     }
 
     /* Pattern of mirrored letter Z. */
-    private static void mirrorLetterZ() {
+    private static String mirrorLetterZ() {
         final int size = 7;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (row % size < 2 && col != size) {
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
                 if (col == row || col == size) {
-                    System.out.println('#');
+                    s += String.format("#%n");
                     break;
                 }
-
-                System.out.print("  ");
+                s += String.format("  ");
             }
         }
+        return s;
     }
 
     /* Pattern of letter Z. */
-    private static void letterZ() {
+    private static String letterZ() {
         final int size = 7;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (row % size < 2 && col != size) {
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
                 if (col == size - row + 1 || col == size) {
-                    System.out.println('#');
+                    s += String.format("#%n");
                     break;
                 }
-
-                System.out.print("  ");
+                s += String.format("  ");
             }
         }
+        return s;
     }
 
     /* Pattern of hourglass. */
-    private static void hourGlass() {
+    private static String hourGlass() {
         final int size = 7;
         final int firstCount = 1;
         boolean crossed = false;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (row % size < 2) {
                     if (col == size) {
-                        System.out.println('#');
+                        s += String.format("#%n");
                         break;
                     }
-
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
                 /* size / 2 + 1 for both odd and even numbers. */
                 if (size / 2 + 1 == row) {
                     crossed = true;
                 }
-
                 if (col == row) {
                     if (crossed) {
-                        System.out.println('#');
+                        s += String.format("#%n");
                         break;
                     }
-
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
                 if (col == size - row + 1) {
                     if (crossed) {
-                        System.out.print("# ");
+                        s += String.format("# ");
                         continue;
                     }
-
-                    System.out.println('#');
+                    s += String.format("#%n");
                     break;
                 }
-
-                System.out.print("  ");
+                s += String.format("  ");
             }
         }
+        return s;
     }
 
     /* Pattern of hourglass inside a box. */
-    private static void hourGlassInBox() {
+    private static String hourGlassInBox() {
         final int size = 7;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (col == firstCount) {
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
                 if (col == size) {
-                    System.out.println('#');
+                    s += String.format("#%n");
                     break;
                 }
-
                 if (row % size < 2) {
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
                 if (col == row) {
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
                 if (col == size - row + 1) {
-                    System.out.print("# ");
+                    s += String.format("# ");
                     continue;
                 }
-
-                System.out.print("  ");
+                s += String.format("  ");
             }
         }
+        return s;
     }
 
     /* Pattern of parallelogram. */
-    private static void parallelogram() {
+    private static String parallelogram() {
         final int size = 8;
         final int firstCount = 1;
-
+        String s = "";
         for (int row = firstCount; row <= size; row++) {
             for (int col = firstCount; col <= size; col++) {
                 if (size == col) {
-                    System.out.println('#');
+                    s += String.format("#%n");
                     break;
                 }
-
                 if (col == firstCount) {
                     for (int spaceCount = 0; spaceCount < size - row; spaceCount++) {
-                        System.out.print(' ');
+                        s += String.format(" ");
                     }
                 }
-
-                System.out.print("# ");
+                s += String.format("# ");
             }
         }
+        return s;
     }
 
     /* Patter of solid square. The simplest pattern employing nested loop. All
      * other patterns are more or less adding extension to it.
      */
-    private static void solidSquare() {
+    private static String solidSquare() {
         final int size = 8;
         final int first = 1;
-
+        String s = "";
         for (int row = first; row <= size; row++) {
             for (int col = first; col <= size; col++) {
                 if (col == size) {
-                    System.out.println('#');
+                    s += String.format("#%n");
                     break;
                 }
-                System.out.print("# ");
+                s += String.format("# ");
             }
         }
+        return s;
     }
 
     /* Pattern of checker board. */
-    private static void checkerBoard() {
+    private static String checkerBoard() {
         final int size = 7;
         final int first = 1;
-
+        String s = "";
         for (int row = first; row <= size; row++) {
             if (row % 2 == 0) {
-                System.out.print(' ');
+                s += String.format(" ");
             }
-
             for (int col = first; col < size; col++) {
-                System.out.print("# ");
+                s += String.format("# ");
             }
-
-            System.out.println('#');
+            s += String.format("#%n");
         }
+        return s;
     }
 }
