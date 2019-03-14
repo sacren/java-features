e74be3685bf46752b8a8caf5a8b0317eaacaef04
Jεan Sacren <sakiwit@gmail.com>
2019-03-14 04:24:34 -0600

    Add Movable interface

---
+++ src/main/java/org/dev/my/Movable.java
@@ -0,0 +1,22 @@
+/**
+ * Movable interface for MovablePoint subclass.
+ *
+ * <p>Public methods:
+ * +moveUp():void
+ * +moveDown():void
+ * +moveLeft():void
+ * +moveRight():void
+ */
+public interface Movable {
+    /** Move up method signature. */
+    void moveUp();
+
+    /** Move down method signature. */
+    void moveDown();
+
+    /** Move left method signature. */
+    void moveLeft();
+
+    /** Move right method signature. */
+    void moveRight();
+}
