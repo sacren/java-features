30c95a059d9715ea92bfbf91d6784fc7f5856cb8
Jεan Sacren <sakiwit@gmail.com>
2019-03-26 04:41:44 -0600

    Update test driver for Grades class

--- src/main/java/io/github/sacren/TestGrades.java
+++ src/main/java/io/github/sacren/TestGrades.java
@@ -1,152 +1,30 @@
 /**
- * Prompt the user for the number of students in a class (a non-negative
- * integer) and for the grade of each of them (between 0 and 100).  Compute and
- * print the average (double rounded to 2 decimal places) as well as the lowest
- * and highest grades.  Add standard deviation and both vertical and horizontal
- * histograms.
+ * Test driver for Grades class.
  */
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TestGrades {
-    /** Prompt for input and display results. */
+    /** Test Grades instance and histogram. */
     public static void main(String[] args) {
-        Scanner inStream = new Scanner(System.in);
-        int size;
-        int grade;
+        Scanner in = new Scanner(System.in);
         System.out.print("Enter the number of students: ");
-        size = inStream.nextInt();
-        if (size < 0) {
-            throw new IllegalArgumentException(
-                    String.format("%d is invalid.", size));
-        }
-
-        if (size == 0) {
-            System.out.println("No students, no grades.");
-            inStream.close();
-            return;
-        }
-
-        int[] gradeArray = new int[size];
+        int size = in.nextInt();
+        int grade;
+        Grades grades = new Grades(size);
         for (int i = 0; i < size; i++) {
-            System.out.printf("Enter the grade of student %d: ", i + 1);
-            grade = inStream.nextInt();
-            if (grade < 0 || grade > 100) {
-                throw new IllegalArgumentException(
-                        String.format("%d is invalid!", grade));
-            }
-            gradeArray[i] = grade;
+            System.out.printf("Enter the grade of Student %d: ", i + 1);
+            grade = in.nextInt();
+            grades.addGrade(i, grade); /* add grades one by one */
         }
-
-        System.out.printf(
-                "List of grades is %s%n"
-                        + "Average is %.2f%n"
-                        + "Minimum is %d%n"
-                        + "Maximum is %d%n"
-                        + "Standard deviation is %.2f%n"
-                        + "Histogram of grades:%n",
-                Arrays.toString(gradeArray), getAverage(gradeArray), getMin(gradeArray),
-                getMax(gradeArray), getStdDeviation(gradeArray));
-        int[] histogramArray = getHistogram(gradeArray);
-        int index = 0;
-        for (int count: histogramArray) {
-            System.out.printf(
-                    "%2d - %3d: %s%n",
-                    index * 10,
-                    index == 9 ? index * 10 + 10 : index * 10 + 9,
-                    addAsterisk(count));
-            index++;
-        }
-        final int maxAsterisk = getMax(histogramArray);
-        for (int row = 0; row < maxAsterisk; row++) {
-            index = 0;
-            for (int count: histogramArray) {
-                if (count >= maxAsterisk - row) {
-                    if (index == 0) {
-                        System.out.printf("%3c%3c", '*', ' ');
-                    } else {
-                        System.out.printf("%2c%4c", '*', ' ');
-                    }
-                } else {
-                    System.out.printf("%6c", ' ');
-                }
-                if (index++ == 9) {
-                    System.out.println();
-                }
-            }
-        }
-        /* Deal with the end bins easily outside of the loop. */
-        System.out.printf("%2d-%-2d", 0, 9);
-        for (int i = 1; i < 9; i++) {
-            System.out.printf("%2d-%-3d", i * 10, i * 10 + 9);
-        }
-        System.out.printf("%2d-%-3d%n", 90, 100);
-        inStream.close();
-    }
-
-    private static double getAverage(int[] a) {
-        int sum = 0;
-        for (int element: a) {
-            sum += element;
-        }
-        return (double) sum / a.length;
-    }
-
-    private static int getMin(int[] a) {
-        int min = a[0];
-        for (int number: a) {
-            if (min > number) {
-                min = number;
-            }
-        }
-        return min;
-    }
-
-    private static int getMax(int[] a) {
-        int max = a[0];
-        for (int number: a) {
-            if (max < number) {
-                max = number;
-            }
-        }
-        return max;
-    }
-
-    private static double getStdDeviation(int[] a) {
-        final int size = a.length;
-        int sumOfSquare = 0;
-        int sum = 0;
-        for (int element: a) {
-            sumOfSquare += element * element;
-            sum += element;
-        }
-        return Math.sqrt((double) sumOfSquare / size
-                        - ((double) sum / size) * ((double) sum / size));
-    }
-
-    private static int[] getHistogram(int[] grades) {
-        int[] bins = new int[10]; /* 0..9, 10..19, ... 90..100 */
-        for (int grade: grades) {
-            if (grade == 100) {
-                grade = 90; /* 100 belongs to 90..99 group */
-            }
-            bins[grade / 10]++; /* Initial values are all 0's, then increment. */
-        }
-        return bins;
-    }
-
-    private static String addAsterisk(int count) {
-        String s = "";
-        /* 0 count: the check and loop are skipped.
-         * 1 count: loop is skipped.
-         * 2 and above: loop through.
-         */
-        if (count > 0) {
-            s = "*";
-        }
-        for (int i = 1; i < count; i++) {
-            s += " *";
-        }
-        return s;
+        in.close();
+        grades.init();
+        System.out.println();
+        System.out.println(grades);
+        System.out.println();
+        System.out.println("=== Vertical histogram ===");
+        System.out.println(grades.vertHisto());
+        System.out.println();
+        System.out.println("=== Horizontal histogram ===");
+        System.out.println(grades.flatHisto());
     }
 }
