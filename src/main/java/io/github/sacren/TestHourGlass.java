48a20759aaa0356eed3989fb4e209696028d0df7
Jεan Sacren <sakiwit@gmail.com>
2019-03-28 04:08:24 -0600

    Update test driver for HourGlass class

--- src/main/java/io/github/sacren/TestHourGlass.java
+++ src/main/java/io/github/sacren/TestHourGlass.java
@@ -1,4 +1,5 @@
-/** Print the pattern of hourglass.
+/**
+ * Test driver for HourGlass class.
  *
  * <p># # # # # # #
  *    #           #
@@ -13,56 +14,10 @@
  *    # # # # # # #
  */
 public class TestHourGlass {
-    /** Hourglass pattern. */
+    /** Test driver for hourglass pattern. */
     public static void main(String[] args) {
-        final int height = 11;
-        final int width = 7;
-        final int rowColAlignment = (height - width) / 2;
-        final int firstCount = 1;
-        boolean crossed = false;
-
-        for (int row = firstCount; row <= height; row++) {
-            for (int col = firstCount; col <= width; col++) {
-                if (row % height < 2) {
-                    if (col == width) {
-                        System.out.println('#');
-                        break;
-                    }
-                    System.out.print("# ");
-                    continue;
-                }
-                if (row % height <= rowColAlignment || row % height >= height - rowColAlignment) {
-                    if (col == width) {
-                        System.out.println('#');
-                        break;
-                    }
-                    if (col == firstCount) {
-                        System.out.print("# ");
-                        continue;
-                    }
-                }
-                /* height / 2 + 1 for both odd and even numbers. */
-                if (height / 2 + 1 == row) {
-                    crossed = true;
-                }
-                if (col == row - rowColAlignment) {
-                    if (crossed) {
-                        System.out.println('#');
-                        break;
-                    }
-                    System.out.print("# ");
-                    continue;
-                }
-                if (col == height - row + 1 - rowColAlignment) {
-                    if (crossed) {
-                        System.out.print("# ");
-                        continue;
-                    }
-                    System.out.println('#');
-                    break;
-                }
-                System.out.print("  ");
-            }
-        }
+        HourGlass hg = new HourGlass();
+        System.out.println();
+        System.out.print(hg);
     }
 }
