7be413fec08d6473d4f78a4eb63dec74be35bfe7
Jεan Sacren <sakiwit@gmail.com>
2019-04-04 02:31:19 -0600

    Add geometric series to 5000

--- src/main/java/io/github/sacren/TestGeoSeries.java
+++ src/main/java/io/github/sacren/TestGeoSeries.java
@@ -4,10 +4,15 @@
 public class TestGeoSeries {
     /** GeoSeries test dirver. */
     public static void main(String[] args) {
-        GeoSeries sum = new GeoSeries(1000);
+        GeoSeries sum1000 = new GeoSeries(1000);
         System.out.println();
         System.out.println("=== Sum of geometric series to 1000 ===");
         System.out.println();
-        System.out.println(sum);
+        System.out.println(sum1000);
+        GeoSeries sum5000 = new GeoSeries(5000);
+        System.out.println();
+        System.out.println("=== Sum of geometric series to 5000 ===");
+        System.out.println();
+        System.out.println(sum5000);
     }
 }
