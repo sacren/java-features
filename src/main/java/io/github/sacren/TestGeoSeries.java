6919fe915507c6881348641226696d3a45eb7357
Jεan Sacren <sakiwit@gmail.com>
2019-04-23 03:06:26 -0600

    Add bounds of 50000 to GeoSeries driver

--- src/main/java/io/github/sacren/TestGeoSeries.java
+++ src/main/java/io/github/sacren/TestGeoSeries.java
@@ -4,15 +4,20 @@
 public class TestGeoSeries {
     /** GeoSeries test dirver. */
     public static void main(String[] args) {
-        GeoSeries sum1000 = new GeoSeries(1000);
+        /* 1000 */
         System.out.println();
         System.out.println("=== Sum of geometric series to 1000 ===");
         System.out.println();
-        System.out.println(sum1000);
-        GeoSeries sum5000 = new GeoSeries(5000);
+        System.out.println(new GeoSeries(1000));
+        /* 5000 */
         System.out.println();
         System.out.println("=== Sum of geometric series to 5000 ===");
         System.out.println();
-        System.out.println(sum5000);
+        System.out.println(new GeoSeries(5000));
+        /* 50000 but MAX_VALUE takes too long */
+        System.out.println();
+        System.out.println("=== Sum of geometric series to 50000 ===");
+        System.out.println();
+        System.out.println(new GeoSeries(50000));
     }
 }
