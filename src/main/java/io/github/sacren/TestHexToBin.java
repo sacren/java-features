e4ba1c8b60e092adfac244b31c28e90715df5fcd
Jεan Sacren <sakiwit@gmail.com>
2019-04-24 03:53:49 -0600

    Update HexToBin driver

--- src/main/java/io/github/sacren/TestHexToBin.java
+++ src/main/java/io/github/sacren/TestHexToBin.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class TestHexToBin {
     /** HexToBin test driver. */
     public static void main(String[] args) {
-        Scanner inStream = new Scanner(System.in);
+        String hex;
+        Scanner in = new Scanner(System.in);
         System.out.println();
         System.out.print("Enter a hex string: ");
-        String hex = inStream.nextLine();
-        inStream.close();
-        HexToBin bin = new HexToBin(hex);
+        hex = in.nextLine();
+        in.close();
         System.out.println();
-        System.out.println("=== Convert hex string to binary string ===");
+        System.out.println("=== Hex to binary ===");
         System.out.println();
-        System.out.println(bin);
+        System.out.println(new HexToBin(hex));
     }
 }
