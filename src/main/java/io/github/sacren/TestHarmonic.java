80ee2893e4b8aea4867c750a0330b49d5b332165
Jεan Sacren <sakiwit@gmail.com>
2019-04-24 02:45:08 -0600

    Update Harmonic driver

--- src/main/java/io/github/sacren/TestHarmonic.java
+++ src/main/java/io/github/sacren/TestHarmonic.java
@@ -5,9 +5,8 @@ public class TestHarmonic {
     /** Harmonic test driver. */
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("=== Harmonic series ===");
+        System.out.println("=== Harmonic series of 50000 ===");
         System.out.println();
-        Harmonic harmonic = new Harmonic(50000);
-        System.out.println(harmonic);
+        System.out.println(new Harmonic(50000));
     }
 }
