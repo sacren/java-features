2cf1c58843b7c92c8f5fe12eee4006ee8979cf24
Jεan Sacren <sakiwit@gmail.com>
2019-05-02 02:05:29 -0600

    Add LeapYears method for list of leap years

--- src/main/java/io/github/sacren/LeapYears.java
+++ src/main/java/io/github/sacren/LeapYears.java
@@ -1,10 +1,15 @@
 /**
  * LeapYears class for all the leap years between AD1 and AD2020.
  *
+ * <p>Private instance data:
+ * -list:String
+ * -count:int
+ *
  * <p>Constructor:
  * +LeapYears()
  *
  * <p>Public methods:
+ * +calc():void
  * +toString():String
  */
 public class LeapYears {
@@ -12,22 +17,35 @@ public class LeapYears {
     private static final int BEGIN = 1;
     private static final int END = 2020;
 
-    /** LeapYears instance printout. */
-    public String toString() {
-        String s = "";
-        int count = 0;
+    /* private instance data */
+    private String list;
+    private int count;
+
+    /** LeapYears constructor. */
+    public LeapYears() {
+        calc();
+    }
+
+    /** LeapYears method for list of leap years. */
+    public void calc() {
+        list = "";
+        count = 0;
         for (int year = BEGIN; year <= END; year++) {
             if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-                s += String.format("%5d", year);
+                list += String.format("%5d", year);
                 if (++count % 10 == 0) {
-                    s += String.format("%n");
+                    list += String.format("%n");
                 }
             }
         }
+    }
+
+    /** LeapYears instance printout. */
+    public String toString() {
         return String.format(
                 "%s%n"
                         + "Total leap years: %d",
-                s,
+                list,
                 count);
     }
 }
