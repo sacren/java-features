646753004a0762697d518b002b88fa6652f855db
Jεan Sacren <sakiwit@gmail.com>
2019-05-10 04:12:33 -0600

    Use Integer methods for hex to binary conversion

--- src/main/java/io/github/sacren/HexToBin.java
+++ src/main/java/io/github/sacren/HexToBin.java
@@ -1,5 +1,5 @@
 /**
- * HexToBin class.
+ * HexToBin class converts hex string to binary.
  *
  * <p>Private instance data:
  * -hex:String
@@ -8,51 +8,18 @@
  * +HexToBin(hex:String)
  *
  * <p>Public methods:
- * +check():void
- * +toBin():String
  * +toString():String
  */
 public class HexToBin {
+    /* private static data */
+    private static final int RADIX = 16;
+
     /* private instance data */
     private String hex;
 
     /** HexToBin custom constructor. */
     public HexToBin(String hex) {
         this.hex = hex;
-        check();
-    }
-
-    /** HexToBin method to validate input string is hex. */
-    public void check() {
-        if (hex.isEmpty()) {
-            throw new IllegalArgumentException(
-                    String.format(
-                        "\"%s\" is not a hex string!",
-                        hex));
-        }
-        for (char c : hex.toCharArray()) {
-            if (Character.digit(c, 16) == -1) {
-                throw new IllegalArgumentException(
-                        String.format(
-                            "\"%s\" is not a hex string!",
-                            hex));
-            }
-        }
-    }
-
-    /** HexToBin method to convert hex string to binary string. */
-    public String toBin() {
-        String[] bin = {
-            "0000", "0001", "0010", "0011",
-            "0100", "0101", "0110", "0111",
-            "1000", "1001", "1010", "1011",
-            "1100", "1101", "1110", "1111"
-        };
-        String s = "";
-        for (char c : hex.toCharArray()) {
-            s += bin[Character.digit(c, 16)];
-        }
-        return s;
     }
 
     /** HexToBin instance printout. */
@@ -60,6 +27,7 @@ public class HexToBin {
         return String.format(
                 "Hex:    %s%n"
                         + "Binary: %s",
-                hex, toBin());
+                hex,
+                Integer.toBinaryString(Integer.parseUnsignedInt(hex, RADIX)));
     }
 }
