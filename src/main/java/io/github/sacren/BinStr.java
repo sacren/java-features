9e1b72d43d458833d0268d056e8133e26da047fd
Jεan Sacren <sakiwit@gmail.com>
2019-05-10 03:11:40 -0600

    Use parseInt for binary string conversion

--- src/main/java/io/github/sacren/BinStr.java
+++ src/main/java/io/github/sacren/BinStr.java
@@ -5,42 +5,23 @@
  *
  * <p>Private data:
  * -binStr:String
- * -decimal:long
  *
  * <p>Constructor:
  * +BinStr(binStr:String)
  *
  * <p>Public methods:
- * +convert():void
  * +toString():String
  */
 public class BinStr {
+    /* private static data */
+    private static final int RADIX = 2;
+
     /* private instance data */
     private String binStr;
-    private long decimal;
 
     /** BinStr constructor. */
     public BinStr(String binStr) {
         this.binStr = binStr;
-        convert();
-    }
-
-    /** BinStr method to convert to decimal value. */
-    public void convert() {
-        final char[] bin = binStr.toCharArray();
-        final int size = bin.length;
-        int index = 0;
-        int exponent;
-        decimal = 0;
-        for (char c: bin) {
-            if (Character.digit(c, 2) == -1) {
-                throw new IllegalArgumentException(
-                        String.format("\"%s\" is not a binary string!", binStr));
-            }
-            /* (size - 1) is the greatest index and each iteration makes it decrement. */
-            exponent = size - 1 - index++;
-            decimal += (long) Math.pow(2, exponent) * Character.digit(c, 2);
-        }
     }
 
     /** BinStr instance printout. */
@@ -48,6 +29,7 @@ public class BinStr {
         return String.format(
                 "Binary string: %s%n"
                         + "Decimal value: %d",
-                binStr, decimal);
+                binStr,
+                Integer.parseInt(binStr, RADIX));
     }
 }
