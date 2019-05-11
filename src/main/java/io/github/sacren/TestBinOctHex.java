3e1baf7413e66533a33f3fb41df9ac5b444eda89
Jεan Sacren <sakiwit@gmail.com>
2019-05-11 02:38:22 -0600

    Add driver for all BinStr, OctStr and HexToBin

---
+++ src/main/java/io/github/sacren/TestBinOctHex.java
@@ -0,0 +1,33 @@
+/**
+ * Test driver for BinStr, OctStr and HexToBin classes.
+ */
+import java.util.Scanner;
+
+public class TestBinOctHex {
+    /** BinStr, OctStr and HexToBin test driver. */
+    public static void main(String[] args) {
+        String binStr;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a binary string: ");
+        binStr = in.nextLine();
+        String octStr;
+        System.out.print("Enter a octal string: ");
+        octStr = in.nextLine();
+        String hexStr;
+        System.out.print("Enter a hex string: ");
+        hexStr = in.nextLine();
+        in.close();
+        System.out.println();
+        System.out.println("=== Binary string to decimal ===");
+        System.out.println();
+        System.out.println(new BinStr(binStr));
+        System.out.println();
+        System.out.println("=== Octal string to decimal ===");
+        System.out.println();
+        System.out.println(new OctStr(octStr));
+        System.out.println();
+        System.out.println("=== Hex string to binary ===");
+        System.out.println();
+        System.out.println(new HexToBin(hexStr));
+    }
+}
