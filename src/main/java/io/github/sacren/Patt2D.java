f7a3f032f6355213aa1a8a9f354270b51060846b
Jεan Sacren <sakiwit@gmail.com>
2019-05-13 04:26:00 -0600

    Use OOP for 2-D patterns

--- src/main/java/io/github/sacren/Patt2D.java
+++ src/main/java/io/github/sacren/Patt2D.java
@@ -1,5 +1,20 @@
 /**
- * Various 2-D patterns through nested loop.
+ * Patt2D class for 2-D patterns.
+ *
+ * <p>Public methods:
+ * +getEmptySq():String
+ * +getTriBotL():String
+ * +getTriTopL():String
+ * +getTriBotR():String
+ * +getTriTopR():String
+ * +getMirrorZ():String
+ * +getLetterZ():String
+ * +getHrGlass():String
+ * +getHrGlBox():String
+ * +getParall():String
+ * +getSolidSq():String
+ * +getChecker():String
+ * +toString():String
  *
  * <p>#
  *    # #
@@ -115,50 +130,21 @@
  *    # # # # # # #
  *         (l)
  */
-public class TestPatternIn2D {
-    /** Display various 2-D patterns. */
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println("List of various 2-D patterns.");
-        System.out.println();
-        System.out.print(triangleBottomLeft());
-        System.out.printf("%9s%n%n", "(a)");
-        System.out.print(triangleTopLeft());
-        System.out.printf("%9s%n%n", "(b)");
-        System.out.print(triangleTopRight());
-        System.out.printf("%9s%n%n", "(c)");
-        System.out.print(triangleBottomRight());
-        System.out.printf("%9s%n%n", "(d)");
-        System.out.print(emptySquare());
-        System.out.printf("%8s%n%n", "(e)");
-        System.out.print(mirrorLetterZ());
-        System.out.printf("%8s%n%n", "(f)");
-        System.out.print(letterZ());
-        System.out.printf("%8s%n%n", "(g)");
-        System.out.print(hourGlass());
-        System.out.printf("%8s%n%n", "(h)");
-        System.out.print(hourGlassInBox());
-        System.out.printf("%8s%n%n", "(i)");
-        System.out.print(parallelogram());
-        System.out.printf("%9s%n%n", "(j)");
-        System.out.print(solidSquare());
-        System.out.printf("%9s%n%n", "(k)");
-        System.out.print(checkerBoard());
-        System.out.printf("%8s%n", "(l)");
-    }
+public class Patt2D {
+    /* private static data */
+    private static final int SIZE = 8;
+    private static final int FIRST_COUNT = 1;
 
-    /* Pattern of square with empty center. */
-    private static String emptySquare() {
-        final int size = 7;
-        final int firstCount = 1;
+    /** Patt2D getter for pattern of square with empty center. */
+    public String getEmptySq() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (row % size > 1 && col % size > 1) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (row % SIZE > 1 && col % SIZE > 1) {
                     s += String.format("  ");
                     continue;
                 }
-                if (col == size) {
+                if (col == SIZE) {
                     s += String.format("#%n");
                     continue;
                 }
@@ -168,13 +154,11 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of triangle with right angle at bottom left. */
-    private static String triangleBottomLeft() {
-        final int size = 8;
-        final int firstCount = 1;
+    /** Patt2D getter for right triangle at bottom left. */
+    public String getTriBotL() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
                 if (col == row) {
                     s += String.format("#%n");
                     break;
@@ -185,14 +169,12 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of triangle with right angle at top left. */
-    private static String triangleTopLeft() {
-        final int size = 8;
-        final int firstCount = 1;
+    /** Patt2D getter for right triangle at top left. */
+    public String getTriTopL() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (col + row > size) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (col + row > SIZE) {
                     s += String.format("#%n");
                     break;
                 }
@@ -202,14 +184,12 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of triangle with right angle at top right. */
-    private static String triangleTopRight() {
-        final int size = 8;
-        final int firstCount = 1;
+    /** Patt2D getter for right triangle at top right. */
+    public String getTriTopR() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (col == size) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (col == SIZE) {
                     s += String.format("#%n");
                     break;
                 }
@@ -223,18 +203,16 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of triangle with right angle at bottom right. */
-    private static String triangleBottomRight() {
-        final int size = 8;
-        final int firstCount = 1;
+    /** Patt2D getter for right triangle at bottom right. */
+    public String getTriBotR() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (col == size) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (col == SIZE) {
                     s += String.format("#%n");
                     break;
                 }
-                if (col + row <= size) {
+                if (col + row <= SIZE) {
                     s += String.format("  ");
                     continue;
                 }
@@ -244,18 +222,16 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of mirrored letter Z. */
-    private static String mirrorLetterZ() {
-        final int size = 7;
-        final int firstCount = 1;
+    /** Patt2D getter for mirrored letter Z. */
+    public String getMirrorZ() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (row % size < 2 && col != size) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (row % SIZE < 2 && col != SIZE) {
                     s += String.format("# ");
                     continue;
                 }
-                if (col == row || col == size) {
+                if (col == row || col == SIZE) {
                     s += String.format("#%n");
                     break;
                 }
@@ -265,18 +241,16 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of letter Z. */
-    private static String letterZ() {
-        final int size = 7;
-        final int firstCount = 1;
+    /** Patt2D getter for pattern of letter Z. */
+    public String getLetterZ() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (row % size < 2 && col != size) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (row % SIZE < 2 && col != SIZE) {
                     s += String.format("# ");
                     continue;
                 }
-                if (col == size - row + 1 || col == size) {
+                if (col == SIZE - row + 1 || col == SIZE) {
                     s += String.format("#%n");
                     break;
                 }
@@ -286,24 +260,22 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of hourglass. */
-    private static String hourGlass() {
-        final int size = 7;
-        final int firstCount = 1;
+    /** Patt2D getter for pattern of hourglass. */
+    public String getHrGlass() {
         boolean crossed = false;
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (row % size < 2) {
-                    if (col == size) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (row % SIZE < 2) {
+                    if (col == SIZE) {
                         s += String.format("#%n");
                         break;
                     }
                     s += String.format("# ");
                     continue;
                 }
-                /* size / 2 + 1 for both odd and even numbers. */
-                if (size / 2 + 1 == row) {
+                /* SIZE / 2 + 1 for both odd and even numbers. */
+                if (SIZE / 2 + 1 == row) {
                     crossed = true;
                 }
                 if (col == row) {
@@ -314,7 +286,7 @@ public class TestPatternIn2D {
                     s += String.format("# ");
                     continue;
                 }
-                if (col == size - row + 1) {
+                if (col == SIZE - row + 1) {
                     if (crossed) {
                         s += String.format("# ");
                         continue;
@@ -328,22 +300,20 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of hourglass inside a box. */
-    private static String hourGlassInBox() {
-        final int size = 7;
-        final int firstCount = 1;
+    /** Patt2D getter for pattern of hourglass inside a box. */
+    public String getHrGlBox() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (col == firstCount) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (col == FIRST_COUNT) {
                     s += String.format("# ");
                     continue;
                 }
-                if (col == size) {
+                if (col == SIZE) {
                     s += String.format("#%n");
                     break;
                 }
-                if (row % size < 2 || col == row || col == size - row + 1) {
+                if (row % SIZE < 2 || col == row || col == SIZE - row + 1) {
                     s += String.format("# ");
                     continue;
                 }
@@ -353,19 +323,17 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of parallelogram. */
-    private static String parallelogram() {
-        final int size = 8;
-        final int firstCount = 1;
+    /** Patt2D getter for pattern of parallelogram. */
+    public String getParall() {
         String s = "";
-        for (int row = firstCount; row <= size; row++) {
-            for (int col = firstCount; col <= size; col++) {
-                if (size == col) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (SIZE == col) {
                     s += String.format("#%n");
                     break;
                 }
-                if (col == firstCount) {
-                    for (int spaceCount = 0; spaceCount < size - row; spaceCount++) {
+                if (col == FIRST_COUNT) {
+                    for (int spaceCount = 0; spaceCount < SIZE - row; spaceCount++) {
                         s += String.format(" ");
                     }
                 }
@@ -375,16 +343,17 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Patter of solid square. The simplest pattern employing nested loop. All
-     * other patterns are more or less adding extension to it.
+    /**
+     * Patt2D getter for solid square.
+     *
+     * <p>The simplest pattern employing nested loop. All other patterns are
+     * more or less adding extension to it.
      */
-    private static String solidSquare() {
-        final int size = 8;
-        final int first = 1;
+    public String getSolidSq() {
         String s = "";
-        for (int row = first; row <= size; row++) {
-            for (int col = first; col <= size; col++) {
-                if (col == size) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
+            for (int col = FIRST_COUNT; col <= SIZE; col++) {
+                if (col == SIZE) {
                     s += String.format("#%n");
                     break;
                 }
@@ -394,20 +363,47 @@ public class TestPatternIn2D {
         return s;
     }
 
-    /* Pattern of checker board. */
-    private static String checkerBoard() {
-        final int size = 7;
-        final int first = 1;
+    /** Patt2D getter for pattern of checker board. */
+    public String getChecker() {
         String s = "";
-        for (int row = first; row <= size; row++) {
+        for (int row = FIRST_COUNT; row <= SIZE; row++) {
             if (row % 2 == 0) {
                 s += String.format(" ");
             }
-            for (int col = first; col < size; col++) {
+            for (int col = FIRST_COUNT; col < SIZE; col++) {
                 s += String.format("# ");
             }
             s += String.format("#%n");
         }
         return s;
+    }
+
+    /** Patt2D string method. */
+    public String toString() {
+        return String.format(
+                "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s%n%n"
+                        + "%s%9s",
+                getTriBotL(), "(a)",
+                getTriTopL(), "(b)",
+                getTriTopR(), "(c)",
+                getTriBotR(), "(d)",
+                getEmptySq(), "(e)",
+                getMirrorZ(), "(f)",
+                getLetterZ(), "(g)",
+                getHrGlass(), "(h)",
+                getHrGlBox(), "(i)",
+                getParall(), "(j)",
+                getSolidSq(), "(k)",
+                getChecker(), "(l)");
     }
 }
---
+++ src/main/java/io/github/sacren/TestPatt2D.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for Patt2d for 2-D patterns through nested loop.
+ */
+public class TestPatt2D {
+    /** Patt2D test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== 2-D patterns ===");
+        System.out.println();
+        System.out.println(new Patt2D());
+    }
+}
