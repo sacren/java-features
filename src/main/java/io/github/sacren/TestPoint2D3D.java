6ea63922f21b9560e9940d338f58eabfb6ddcf69
Jεan Sacren <sakiwit@gmail.com>
2019-05-15 04:27:14 -0600

    Update message for Point super/sub class driver

--- src/main/java/io/github/sacren/TestPoint2D3D.java
+++ src/main/java/io/github/sacren/TestPoint2D3D.java
@@ -4,13 +4,20 @@
 public class TestPoint2D3D {
     /** Initialize and run Point2D class. */
     public static void main(String[] args) {
+        /* point in 2-D */
         Point2D point2D = new Point2D();
+        System.out.println();
+        System.out.println("=== Point in 2-D ===");
+        System.out.println();
         System.out.println(point2D);
         System.out.printf(
                 "Default Point2D at (%d, %d)%n",
                 point2D.getX(), point2D.getY());
-        System.out.println();
+        /* point in 3-D */
         Point3D point3D = new Point3D();
+        System.out.println();
+        System.out.println("=== Point in 3-D ===");
+        System.out.println();
         System.out.println(point3D);
         System.out.printf(
                 "Default Point3D at (%d, %d, %d)%n",
@@ -24,7 +31,8 @@ public class TestPoint2D3D {
                 customPoint3D.getY(),
                 customPoint3D.getZ());
         System.out.println();
-        System.out.println("After change Point3D:");
+        System.out.println("=== Change to point in 3-D ===");
+        System.out.println();
         customPoint3D.setX(101);
         customPoint3D.setY(102);
         customPoint3D.setZ(103);
