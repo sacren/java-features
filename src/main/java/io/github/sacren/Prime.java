d8407ee504bf8516cfdd89264b1f2fa295f9861b
Jεan Sacren <sakiwit@gmail.com>
2019-05-19 01:23:42 -0600

    Add upper limit for prime number selection

--- src/main/java/io/github/sacren/Prime.java
+++ src/main/java/io/github/sacren/Prime.java
@@ -17,6 +17,9 @@
 import java.util.Arrays;
 
 public class Prime {
+    /* private static data */
+    private static final int MAX_NUM = 10000;
+
     /* private instance data */
     private int num;
     private int count;
@@ -31,9 +34,9 @@ public class Prime {
 
     /** Prime setter for the range. */
     public void setNum(int num) {
-        if (num < 0) {
+        if (num < 0 || num > MAX_NUM) {
             throw new IllegalArgumentException(
-                    String.format("%d is invalid!", num));
+                    String.format("%d is out of range!", num));
         }
         this.num = num;
     }
