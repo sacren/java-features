79cdeeb326872781ce464c3ed152bd86da2f752d
Jεan Sacren <sakiwit@gmail.com>
2019-06-01 04:52:20 -0600

    Add TestString driver

---
+++ src/main/java/io/github/sacren/TestString.java
@@ -0,0 +1,16 @@
+/**
+ * Test driver for StrOp class.
+ */
+public class TestString {
+    /** StrOp and StrConv test drivers. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== String operation ===");
+        System.out.println();
+        System.out.println(new StrOp());
+        System.out.println();
+        System.out.println("=== String conversion ===");
+        System.out.println();
+        System.out.println(new StrConv());
+    }
+}
