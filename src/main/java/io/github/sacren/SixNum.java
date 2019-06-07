1ef709603963008b42337ce2935dedaeeb494b95
Jεan Sacren <sakiwit@gmail.com>
2019-06-07 02:09:46 -0600

    Add SixNum getters for sum and product

--- src/main/java/io/github/sacren/SixNum.java
+++ src/main/java/io/github/sacren/SixNum.java
@@ -1,15 +1,12 @@
 /**
  * SixNum class for sum and product of six integers.
  *
- * <p>Private intace data:
- * -sum:int
- * -product:int
- *
  * <p>Constructor:
  * +SixNum()
  *
  * <p>Public methods:
- * +set():void
+ * +getSum():int
+ * +getProd():int
  * +toString():String
  */
 import java.util.Arrays;
@@ -18,33 +15,32 @@ public class SixNum {
     /* private static data */
     private static final int[] LIST = {11, 22, 33, 44, 55, 66};
 
-    /* private instance data */
-    private int sum;
-    private int product;
-
-    /** SixNum constructor. */
-    public SixNum() {
-        set();
-    }
-
-    /** SixNum setter for sum and product. */
-    public void set() {
-        sum = 0;
-        product = 1;
+    /** SixNum getter for the sum of numbers. */
+    public int getSum() {
+        int sum = 0;
         for (int i : LIST) {
             sum += i;
+        }
+        return sum;
+    }
+
+    /** SixNum getter for the product of numbers. */
+    public int getProd() {
+        int product = 1;
+        for (int i : LIST) {
             product *= i;
         }
+        return product;
     }
 
     /** SixNum string method. */
     public String toString() {
         return String.format(
-                          "List:    %s%n%n"
+                "List:    %s%n%n"
                         + "Sum:     %d%n"
                         + "Product: %d",
                 Arrays.toString(LIST),
-                sum,
-                product);
+                getSum(),
+                getProd());
     }
 }
