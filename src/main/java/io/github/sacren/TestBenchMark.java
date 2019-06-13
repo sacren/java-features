59150af76938433526e3a5c2ada011ab326b7c9f
Jεan Sacren <sakiwit@gmail.com>
2019-06-13 04:17:54 -0600

    Add test driver for BenchMark class

---
+++ src/main/java/io/github/sacren/TestBenchMark.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for BenchMark class.
+ */
+public class TestBenchMark {
+    /** BenchMark test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Bench mark ===");
+        System.out.println();
+        System.out.println(new BenchMark());
+    }
+}
