1c8cd0d2f2b8a5c559c56502a17faac132e234bb
Jεan Sacren <sakiwit@gmail.com>
2019-06-15 03:12:04 -0600

    Add test driver for Token class

---
+++ src/main/java/io/github/sacren/TestToken.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for Token class.
+ */
+public class TestToken {
+    /** Token test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Tokenize ===");
+        System.out.println();
+        System.out.println(new Token());
+    }
+}
