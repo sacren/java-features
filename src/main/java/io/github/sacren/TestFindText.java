34ab1b61f08cf8100ba4c5052a4046eeb8166eeb
Jεan Sacren <sakiwit@gmail.com>
2019-06-20 02:58:05 -0600

    Add test driver for FindText class

---
+++ src/main/java/io/github/sacren/TestFindText.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for FindText class.
+ */
+public class TestFindText {
+    /** FindText test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Regular expression ===");
+        System.out.println();
+        System.out.println(new FindText());
+    }
+}
