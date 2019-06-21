4aae40a31899f0e8280e72ce976ba3c1daf2a98f
Jεan Sacren <sakiwit@gmail.com>
2019-06-21 01:08:13 -0600

    Add BackRef test driver

---
+++ src/main/java/io/github/sacren/TestBackRef.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for BackRef class.
+ */
+public class TestBackRef {
+    /** BackRef test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Back reference ===");
+        System.out.println();
+        System.out.println(new BackRef());
+    }
+}
