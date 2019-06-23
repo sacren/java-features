9be01494dcae5e39f73a264e8530c0ce64769538
Jεan Sacren <sakiwit@gmail.com>
2019-06-23 02:05:01 -0600

    Add Split class and driver to tokenize string

---
+++ src/main/java/io/github/sacren/Split.java
@@ -0,0 +1,49 @@
+/**
+ * Split class to split string by regex.
+ *
+ * <p>Private instance data:
+ * -input:String
+ * -output:String
+ *
+ * <p>Constructor:
+ * +Split()
+ *
+ * <p>Public methods:
+ * +set():void
+ * +toString():String
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Split {
+    /* private instance data */
+    private String input;
+    private String output;
+
+    /** Split constructor. */
+    public Split() {
+        input = "This is an apple. These are 33 (thirty-three) apples.";
+        set();
+    }
+
+    /** Split setter to tokenize string. */
+    public void set() {
+        StringBuilder sb = new StringBuilder();
+        for (String token : input.split("\\s+|-")) {
+            sb.append(token);
+            sb.append(String.format("%n"));
+        }
+        output = sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+    /** Split string method. */
+    public String toString() {
+        return String.format(
+                "%n=== Original text ===%n%n"
+                        + "%s%n%n"
+                        + "=== After split ===%n%n"
+                        + "%s",
+                input,
+                output);
+    }
+}
---
+++ src/main/java/io/github/sacren/TestSplit.java
@@ -0,0 +1,9 @@
+/**
+ * Test driver for Split class.
+ */
+public class TestSplit {
+    /** Split test driver. */
+    public static void main(String[] args) {
+        System.out.println(new Split());
+    }
+}
