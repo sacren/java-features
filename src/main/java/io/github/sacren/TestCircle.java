ac5afab728ae8a5f78b9bb6b80e51c0a086b1fe4
Jεan Sacren <sakiwit@gmail.com>
2019-07-02 02:23:19 -0600

    Add test driver for Circle class

---
+++ src/main/java/io/github/sacren/TestCircle.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for Circle class.
+ */
+public class TestCircle {
+    /** Circle test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Circle ===");
+        System.out.println();
+        System.out.println(new Circle());
+    }
+}
