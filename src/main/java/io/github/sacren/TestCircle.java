fe08e516bb340a5e40025ba9d4902917f327f943
Jεan Sacren <sakiwit@gmail.com>
2019-07-02 03:34:08 -0600

    Add a pink circle for Circle instance

--- src/main/java/io/github/sacren/TestCircle.java
+++ src/main/java/io/github/sacren/TestCircle.java
@@ -5,8 +5,13 @@ public class TestCircle {
     /** Circle test driver. */
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("=== Circle ===");
+        System.out.println("=== Default circle ===");
         System.out.println();
         System.out.println(new Circle());
+        System.out.println();
+        System.out.println("=== A pink circle ===");
+        System.out.println();
+        Point center = new Point(45, 101);
+        System.out.println(new Circle(center, 6, "pink"));
     }
 }
