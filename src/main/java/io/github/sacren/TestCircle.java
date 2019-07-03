c988884fdd495709f7539913be2e10b343270634
Jεan Sacren <sakiwit@gmail.com>
2019-07-03 02:56:33 -0600

    Add default circle instance by Shape class

--- src/main/java/io/github/sacren/TestCircle.java
+++ src/main/java/io/github/sacren/TestCircle.java
@@ -9,6 +9,24 @@ public class TestCircle {
         System.out.println();
         /* default circle */
         System.out.println(new Circle());
+        /* default circle by Shape */
+        System.out.println();
+        System.out.println("=== Default circle by Shape ===");
+        System.out.println();
+        Shape c1 = new Circle();
+        System.out.println(c1);
+        System.out.println();
+        System.out.println("=== Calling default circle methods ===");
+        System.out.println();
+        System.out.printf(
+                "Circle area:          %.2f%n"
+                        + "Circle circumference: %.2f%n"
+                        + "Circle color:         %s%n"
+                        + "Circle center:        %s%n",
+                c1.getArea(),
+                c1.getPerimeter(),
+                c1.getColor(),
+                ((Circle)c1).getCenter());
         System.out.println();
         System.out.println("=== A pink circle ===");
         System.out.println();
