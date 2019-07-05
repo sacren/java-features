5305257e61ce21bd6e89d3c8193ebb05e21203b2
Jεan Sacren <sakiwit@gmail.com>
2019-07-05 01:07:33 -0600

    Add test driver for Rectangle class

---
+++ src/main/java/io/github/sacren/TestRectangle.java
@@ -0,0 +1,13 @@
+/**
+ * Test driver for Rectangle class.
+ */
+public class TestRectangle {
+    /** Circle test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Default rectangle ===");
+        System.out.println();
+        /* default rectangle */
+        System.out.println(new Rectangle());
+    }
+}
