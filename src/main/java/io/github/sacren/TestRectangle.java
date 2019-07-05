64a25b01d0f47bdddabef844580c5d46eb485c44
Jεan Sacren <sakiwit@gmail.com>
2019-07-05 04:43:45 -0600

    Add Rectangle instance with length only

--- src/main/java/io/github/sacren/TestRectangle.java
+++ src/main/java/io/github/sacren/TestRectangle.java
@@ -8,16 +8,23 @@ public class TestRectangle {
         System.out.println("=== Default rectangle ===");
         System.out.println();
         /* default rectangle */
-        Rectangle r = new Rectangle();
-        System.out.println(r);
+        Rectangle r1 = new Rectangle();
+        System.out.println(r1);
         System.out.println();
         System.out.println("=== Rectangle with place holder ===");
         System.out.println();
-        System.out.println(new Rectangle(r));
+        System.out.println(new Rectangle(r1));
         System.out.println();
         System.out.println(new Rectangle(null));
         System.out.println();
         Shape s = new Rectangle();
         System.out.println(new Rectangle((Rectangle) s));
+        Rectangle r2 = new Rectangle(10);
+        System.out.println();
+        System.out.println(new Rectangle(r2));
+        System.out.println();
+        System.out.println("=== Rectangle with length ===");
+        System.out.println();
+        System.out.println(r2);
     }
 }
