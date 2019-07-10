f84929431a47307326782ec3a80c0a06d4ae71d6
Jεan Sacren <sakiwit@gmail.com>
2019-07-10 03:38:06 -0600

    Add Triangle driver for default triangle

---
+++ src/main/java/io/github/sacren/TestTriangle.java
@@ -0,0 +1,13 @@
+/**
+ * Test driver for Triangle class.
+ */
+public class TestTriangle {
+    /** Triangle test driver. */
+    public static void main(String[] args) {
+        /* default Triangle */
+        System.out.println();
+        System.out.println("=== Default Triangle ===");
+        System.out.println();
+        System.out.println(new Triangle());
+    }
+}
