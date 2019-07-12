a181c97c33dd649511640bf64a84f72ea09314a4
Jεan Sacren <sakiwit@gmail.com>
2019-07-12 01:22:08 -0600

    Add Cylinder driver for default cylinder

---
+++ src/main/java/io/github/sacren/TestCylinder.java
@@ -0,0 +1,13 @@
+/**
+ * Test driver for Cylinder class.
+ */
+public class TestCylinder {
+    /** Cylinder test driver. */
+    public static void main(String[] args) {
+        /* default Triangle */
+        System.out.println();
+        System.out.println("=== Default cylinder ===");
+        System.out.println();
+        System.out.println(new Cylinder());
+    }
+}
