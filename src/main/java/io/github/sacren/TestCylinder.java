8c61bdd06bc1ac00a37d503129ac2c67e68faec9
Jεan Sacren <sakiwit@gmail.com>
2019-07-12 04:52:27 -0600

    Update cylinder of Cylinder by inheritance

--- src/main/java/io/github/sacren/TestCylinder.java
+++ src/main/java/io/github/sacren/TestCylinder.java
@@ -11,7 +11,15 @@ public class TestCylinder {
         Cylinder c1 = new Cylinder();
         System.out.println(c1);
         System.out.println();
-        System.out.println("=== Default cylinder by Shape ===");
+        System.out.println("=== Update cylinder of Cylinder ===");
+        System.out.println();
+        /* inheritance */
+        c1.setRadius(c1.getRadius() + 1);
+        c1.setHeight(c1.getHeight() + 1);
+        c1.setColor("black");
+        System.out.println(c1);
+        System.out.println();
+        System.out.println("=== Default cylinder of Shape ===");
         System.out.println();
         /* default Cylinder by Shape */
         Shape c2 = new Cylinder();
