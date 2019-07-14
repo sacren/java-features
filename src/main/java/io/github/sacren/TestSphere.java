c13ea15d51eef7f531a07a7f4c5a912cfa8966d1
Jεan Sacren <sakiwit@gmail.com>
2019-07-14 01:27:07 -0600

    Add Sphere driver for default sphere

---
+++ src/main/java/io/github/sacren/TestSphere.java
@@ -0,0 +1,13 @@
+/**
+ * Test driver for Sphere class.
+ */
+public class TestSphere {
+    /** Sphere test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Default sphere ===");
+        System.out.println();
+        /* default sphere */
+        System.out.println(new Sphere());
+    }
+}
