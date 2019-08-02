40f91e187dab703d930bdafb600bbf1b0de639cc
Jεan Sacren <sakiwit@gmail.com>
2019-08-02 02:40:06 -0600

    Clean up obsolete methods of PrimWrap class

--- src/main/java/io/github/sacren/PrimWrap.java
+++ src/main/java/io/github/sacren/PrimWrap.java
@@ -2,46 +2,32 @@
  * PrimWrap class.
  *
  * <p>Private instance data:
- * -num:int
- * -str:String
+ * -intObj:Integer
+ * -strObj:Integer
  *
  * <p>Constructor:
  * +PrimWrap()
  *
  * <p>Public methods:
- * +getInt():Integer
- * +getIntStr():Integer
  * +toString():String
  */
 
 public class PrimWrap {
     /* private instance data */
-    private final int num = 5566;
-    private String str = "5566";
     private Integer intObj;
     private Integer strObj;
 
     /** PrimWrap constructor. */
     public PrimWrap() {
-        intObj = new Integer(num);
-        strObj = new Integer(str);
-    }
-
-    /** PrimWrap getter for integer object. */
-    public Integer getInt() {
-        return new Integer(num);
-    }
-
-    /** PrimWrap getter for integer object by string. */
-    public Integer getIntStr() {
-        return new Integer(str);
+        intObj = new Integer(2019);
+        strObj = new Integer("2019");
     }
 
     /** PrimWrap string method. */
     public String toString() {
         return String.format(
-                "Integer object by int:    %d%n"
-                        + "Integer object by string: %d",
+                "Integer object (int):    %d%n"
+                        + "Integer object (String): %d",
                 intObj,
                 strObj);
     }
