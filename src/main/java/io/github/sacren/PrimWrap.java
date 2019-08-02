dae9d778074c13a269ffe584769b15ccf33ef7ac
Jεan Sacren <sakiwit@gmail.com>
2019-08-02 03:58:41 -0600

    Add more wrapper classes to PrimWrap

--- src/main/java/io/github/sacren/PrimWrap.java
+++ src/main/java/io/github/sacren/PrimWrap.java
@@ -4,6 +4,9 @@
  * <p>Private instance data:
  * -intObj:Integer
  * -strObj:Integer
+ * -dblObj:Double
+ * -charObj:Character
+ * -boolObj:Boolean
  *
  * <p>Constructor:
  * +PrimWrap()
@@ -16,19 +19,31 @@ public class PrimWrap {
     /* private instance data */
     private Integer intObj;
     private Integer strObj;
+    private Double dblObj;
+    private Character charObj;
+    private Boolean boolObj;
 
     /** PrimWrap constructor. */
     public PrimWrap() {
         intObj = new Integer(2019);
         strObj = new Integer("2019");
+        dblObj = new Double(Math.PI);
+        charObj = new Character('X');
+        boolObj = new Boolean(true);
     }
 
     /** PrimWrap string method. */
     public String toString() {
         return String.format(
                 "Integer object (int):    %d%n"
-                        + "Integer object (String): %d",
+                        + "Integer object (String): %d%n"
+                        + "Double object:           %f%n"
+                        + "Character object:        %c%n"
+                        + "Boolean object:          %b",
                 intObj,
-                strObj);
+                strObj,
+                dblObj,
+                charObj,
+                boolObj);
     }
 }
