793e32375239aff25664b71c5c89205b3698dfc0
Jεan Sacren <sakiwit@gmail.com>
2019-08-03 03:39:09 -0600

    Add Short wrapper class to PrimWrap

--- src/main/java/io/github/sacren/PrimWrap.java
+++ src/main/java/io/github/sacren/PrimWrap.java
@@ -2,13 +2,15 @@
  * PrimWrap class.
  *
  * <p>Private instance data:
+ * -byteObj:Byte
+ * -byteObjStr:Byte
+ * -shortObj:Short
+ * -shortObjStr:Short
  * -intObj:Integer
  * -intObjStr:Integer
  * -dblObj:Double
  * -dblObjStr:Double
  * -charObj:Character
- * -byteObj:Byte
- * -byteObjStr:Byte
  * -boolObj:Boolean
  * -boolObjStr:Boolean
  *
@@ -18,29 +20,32 @@
  * <p>Public methods:
  * +toString():String
  */
-import static java.lang.Byte.MAX_VALUE;
 
 public class PrimWrap {
     /* private instance data */
+    private Byte byteObj;
+    private Byte byteObjStr;
+    private Short shortObj;
+    private Short shortObjStr;
     private Integer intObj;
     private Integer intObjStr;
     private Double dblObj;
     private Double dblObjStr;
     private Character charObj;
-    private Byte byteObj;
-    private Byte byteObjStr;
     private Boolean boolObj;
     private Boolean boolObjStr;
 
     /** PrimWrap constructor. */
     public PrimWrap() {
+        byteObj = new Byte(Byte.MAX_VALUE);
+        byteObjStr = new Byte(byteObj.toString());
+        shortObj = new Short(Short.MAX_VALUE);
+        shortObjStr = new Short(shortObj.toString());
         intObj = new Integer(2019);
         intObjStr = new Integer("2019");
         dblObj = new Double(Math.PI);
         dblObjStr = new Double(dblObj.toString());
         charObj = new Character('X');
-        byteObj = new Byte(MAX_VALUE);
-        byteObjStr = new Byte(byteObj.toString());
         boolObj = new Boolean(true);
         boolObjStr = new Boolean(boolObj.toString());
     }
@@ -48,22 +53,26 @@ public class PrimWrap {
     /** PrimWrap string method. */
     public String toString() {
         return String.format(
-                "Integer object (int):     %d%n"
+                "Byte object (int):        %d%n"
+                        + "Byte object (String):     %d%n"
+                        + "Short object (int):       %d%n"
+                        + "Short object (String):    %d%n"
+                        + "Integer object (int):     %d%n"
                         + "Integer object (String):  %d%n"
                         + "Double object (double):   %f%n"
                         + "Double object (String):   %f%n"
                         + "Character object:         %c%n"
-                        + "Byte object (byte):       %d%n"
-                        + "Byte object (String):     %d%n"
                         + "Boolean object (boolean): %b%n"
                         + "Boolean object (String):  %b",
+                byteObj,
+                byteObjStr,
+                shortObj,
+                shortObjStr,
                 intObj,
                 intObjStr,
                 dblObj,
                 dblObjStr,
                 charObj,
-                byteObj,
-                byteObjStr,
                 boolObj,
                 boolObjStr);
     }
