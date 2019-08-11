325bcc064edcbe322e44e3c07a7539b07a1b1170
Jεan Sacren <sakiwit@gmail.com>
2019-08-11 04:57:13 -0600

    Add Cube class to extend Point class

---
+++ src/main/java/io/github/sacren/Cube.java
@@ -0,0 +1,56 @@
+/**
+ * Cube class extends Point class.
+ *
+ * <p>Private data with default value:
+ * -axisZ:double = 0
+ *
+ * <p>Constructor:
+ * +Cube()
+ * +Cube(axisX:double, axisY:double, axisZ:double)
+ *
+ * <p>Public methods:
+ * +getZ():double
+ * +setDefZ():void
+ * +setZ(axisZ:double):void
+ * +toString():String
+ */
+public class Cube extends Point {
+    /* private instance data */
+    private double axisZ;
+
+    /** Cube default constructor. */
+    public Cube() {
+        /* super(); */
+        /* above implicitly executed for default class */
+        setDefZ();
+    }
+
+    /** Cube custom constructor. */
+    public Cube(double axisX, double axisY, double axisZ) {
+        super(axisX, axisY);
+        setZ(axisZ);
+    }
+
+    /** Cube getter for axisZ. */
+    public double getZ() {
+        return axisZ;
+    }
+
+    /** Cube setter for default value of axisZ. */
+    public void setDefZ() {
+        axisZ = 0;
+    }
+
+    /** Cube setter for axisZ. */
+    public void setZ(double axisZ) {
+        this.axisZ = axisZ;
+    }
+
+    /** Cube string method. */
+    @Override
+    public String toString() {
+        return String.format(
+                "(%.2f, %.2f, %.2f)",
+                getX(), getY(), axisZ);
+    }
+}
