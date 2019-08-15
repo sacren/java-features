ccde824df28f590f15d0b0c71da986c1d7cd1037
Jεan Sacren <sakiwit@gmail.com>
2019-08-15 01:23:23 -0600

    Add ListDir and driver for directory contents

---
+++ src/main/java/io/github/sacren/ListDir.java
@@ -0,0 +1,47 @@
+/**
+ * ListDir class for the contents of directory.
+ *
+ * <p>Private instance data:
+ * -dir:File
+ *
+ * <p>Constructor:
+ * +ListDir()
+ *
+ * <p>Public methods:
+ * +getList():String
+ * +toString():String
+ */
+import java.io.File;
+
+public class ListDir {
+    /* private instance data */
+    private File dir;
+
+    /** ListDir constructor. */
+    public ListDir() {
+        dir = new File("/tmp");
+    }
+
+    /** ListDir getter for directory contents. */
+    public String getList() {
+        StringBuilder sb = new StringBuilder();
+        boolean once = true;
+        for (String name : dir.list()) {
+            if (once) {
+                once = false;
+                sb.append(name);
+                continue;
+            }
+            sb.append(String.format("%n")).append(name);
+        }
+        return sb.toString();
+    }
+
+    /** ListDir string method. */
+    public String toString() {
+        return String.format(
+                "=== List directory ===%n%n"
+                        + "%s",
+                getList());
+    }
+}
---
+++ src/main/java/io/github/sacren/TestListDir.java
@@ -0,0 +1,12 @@
+/**
+ * Test driver for ListDir class.
+ */
+import static java.lang.System.out;
+
+public class TestListDir {
+    /** ListDir test driver. */
+    public static void main(String[] args) {
+        out.println();
+        out.println(new ListDir());
+    }
+}
