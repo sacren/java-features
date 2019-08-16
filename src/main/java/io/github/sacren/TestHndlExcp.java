573053e11920a440ee5012d02441bec3c865cd84
Jεan Sacren <sakiwit@gmail.com>
2019-08-16 04:48:45 -0600

    Add FileNotFoundException message

--- src/main/java/io/github/sacren/TestHndlExcp.java
+++ src/main/java/io/github/sacren/TestHndlExcp.java
@@ -13,12 +13,16 @@ public class TestHndlExcp {
             HndlExcp excp = new HndlExcp();
             out.println(excp);
         } catch (FileNotFoundException fnf) {
+            out.println("=== The catch block ===");
+            out.println();
             fnf.printStackTrace();
+            out.println();
+            out.println(fnf.getMessage());
         } finally {
             out.println();
-            out.println("The finally block");
+            out.println("=== The finally block ===");
         }
         out.println();
-        out.println("Outside try-catch-finally block");
+        out.println("=== Outside try-catch-finally ===");
     }
 }
