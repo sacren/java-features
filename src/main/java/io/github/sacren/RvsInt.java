279c3d52adbc5ece18f90922e86718f30b7794e1
Jεan Sacren <sakiwit@gmail.com>
2019-08-25 01:41:30 -0600

    Revert "Use Integer object rather than primitive int"
    This reverts commit bff70137a1771d4e90b53ebb4759472968a97d1e.

--- src/main/java/io/github/sacren/RvsInt.java
+++ src/main/java/io/github/sacren/RvsInt.java
@@ -2,7 +2,7 @@
  * RvsInt class for reversed integer number and sum.
  *
  * <p>Private instance data:
- * -num:Integer
+ * -num:int
  *
  * <p>Constructor:
  * +RvsInt(num:int)
@@ -14,23 +14,23 @@
  */
 public class RvsInt {
     /* private instance data */
-    private Integer num;
+    private int num;
 
     /** RvsInt constructor. */
     public RvsInt(int num) {
-        this.num = new Integer(num);
+        this.num = num;
     }
 
     /** RvsInt getter for the reversed integer. */
     public int getRvs() {
-        StringBuffer sb = new StringBuffer(num.toString());
+        StringBuffer sb = new StringBuffer(Integer.toString(num));
         return Integer.parseUnsignedInt(sb.reverse().toString());
     }
 
     /** RvsInt getter for sum of digits. */
     public int getSum() {
         int sum = 0;
-        for (char c : num.toString().toCharArray()) {
+        for (char c : Integer.toString(num).toCharArray()) {
             sum += Integer.parseUnsignedInt(Character.toString(c));
         }
         return sum;
@@ -42,7 +42,7 @@ public class RvsInt {
                 "Original input: %d%n"
                         + "Reversed:       %d%n"
                         + "Sum of digits:  %d",
-                num.intValue(),
+                num,
                 getRvs(),
                 getSum());
     }
