e48833fdeb4c3cde8f676a8064195819a226202d
Jεan Sacren <sakiwit@gmail.com>
2019-10-17 01:30:38 -0600

    Add handling of int instance variable to Trio class

--- src/main/java/io/github/sacren/Trio.java
+++ src/main/java/io/github/sacren/Trio.java
@@ -4,21 +4,26 @@
  * <p>We use Trio for 3-D pointers.  Broadly Trio could represent any object
  * that requires conceptual three dimensions.  See Time class for details.
  *
- * <p>Private variable with default value:
+ * <p>Private instance variables with default value:
+ * -thirdInt:int = 0
  * -thirdDbl:double = 0
  *
  * <p>Constructor:
  * +Trio()
+ * +Trio(firstInt:int, secondInt:int, thirdInt:int)
  * +Trio(firstDbl:double, secondDbl:double, thirdDbl:double)
  *
  * <p>Public methods:
- * +getThirdDbl():double
  * +setThird():void
+ * +setThird(thirdInt:int):void
  * +setThird(thirdDbl:double):void
+ * +getThirdInt():int
+ * +getThirdDbl():double
  * +toString():String
  */
 public class Trio extends Duo {
-    /* private instance variable */
+    /* private instance variables */
+    private int thirdInt;
     private double thirdDbl;
 
     /** Trio default constructor. */
@@ -28,23 +33,40 @@ public class Trio extends Duo {
         setThird();
     }
 
-    /** Trio custom constructor. */
+    /** Trio constructor for int instance variable. */
+    public Trio(int firstInt, int secondInt, int thirdInt) {
+        super(firstInt, secondInt);
+        setThird(thirdInt);
+    }
+
+    /** Trio constructor for double instance variable. */
     public Trio(double firstDbl, double secondDbl, double thirdDbl) {
         super(firstDbl, secondDbl);
         setThird(thirdDbl);
     }
 
-    /** Trio setter for default thirdDbl instance variable. */
+    /** Trio setter for default instance variables. */
     public void setThird() {
+        thirdInt = 0;
         thirdDbl = 0;
     }
 
-    /** Trio setter for thirdDbl instance variable. */
+    /** Trio setter for int instance variable. */
+    public void setThird(int thirdInt) {
+        this.thirdInt = thirdInt;
+    }
+
+    /** Trio setter for double instance variable. */
     public void setThird(double thirdDbl) {
         this.thirdDbl = thirdDbl;
     }
 
-    /** Trio getter for thirdDbl. */
+    /** Trio getter for int instance variable. */
+    public double getThirdInt() {
+        return thirdInt;
+    }
+
+    /** Trio getter for double instance variable. */
     public double getThirdDbl() {
         return thirdDbl;
     }
