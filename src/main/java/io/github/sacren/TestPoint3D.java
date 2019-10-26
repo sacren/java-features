1be9c5699e5b05f3beeec1b9bf10af7c664c17c0
Jεan Sacren <sakiwit@gmail.com>
2019-10-26 02:27:19 -0600

    Update 3-D point driver with Trio object

--- src/main/java/io/github/sacren/TestPoint3D.java
+++ src/main/java/io/github/sacren/TestPoint3D.java
@@ -1,8 +1,8 @@
 /**
- * Test driver for Point and Cube classes.
+ * Test driver for Point and Trio classes.
  */
 public class TestPoint3D {
-    /** Cube test driver. */
+    /** Point and Trio test driver. */
     public static void main(String[] args) {
         /* point in 2-D */
         Point p2D = new Point();
@@ -16,42 +16,42 @@ public class TestPoint3D {
                 p2D.getX(),
                 p2D.getY());
         /* point in 3-D */
-        Cube p3D = new Cube();
+        Trio p3D = new Trio();
         System.out.println();
         System.out.println("=== Point in 3-D ===");
         System.out.println();
         System.out.printf(
                 "Default 3-D point: P%s%n%n"
-                        + "Default 3-D point: P(%.2f, %.2f, %.2f)%n",
+                        + "Default 3-D point: P(%d, %d, %d)%n",
                 p3D,
-                p3D.getX(),
-                p3D.getY(),
-                p3D.getZ());
+                p3D.getFirstInt(),
+                p3D.getSecondInt(),
+                p3D.getThirdInt());
         /* custom 3-D point */
         System.out.println();
         System.out.println("=== Custom 3-D point ===");
         System.out.println();
-        Cube custP3D = new Cube(1, 2, 3);
+        Trio dbl3D = new Trio((double)1, (double)2, (double)3);
         System.out.printf(
                 "Custom 3-D point: P%s%n%n"
-                        + "Custom 3-D point: P(%.2f, %.2f, %.2f)%n",
-                custP3D,
-                custP3D.getX(),
-                custP3D.getY(),
-                custP3D.getZ());
+                        + "Update 3-D point: P(%.2f, %.2f, %.2f)%n",
+                dbl3D,
+                dbl3D.getFirstDbl(),
+                dbl3D.getSecondDbl(),
+                dbl3D.getThirdDbl());
         /* update custom 3-D point */
         System.out.println();
         System.out.println("=== Change to 3-D point ===");
         System.out.println();
-        custP3D.setX(101);
-        custP3D.setY(102);
-        custP3D.setZ(103);
+        dbl3D.setFirst((double)101);
+        dbl3D.setSecond((double)102);
+        dbl3D.setThird((double)103);
         System.out.printf(
                 "Update 3-D point: P%s%n%n"
                         + "Update 3-D point: P(%.2f, %.2f, %.2f)%n",
-                custP3D,
-                custP3D.getX(),
-                custP3D.getY(),
-                custP3D.getZ());
+                dbl3D,
+                dbl3D.getFirstDbl(),
+                dbl3D.getSecondDbl(),
+                dbl3D.getThirdDbl());
     }
 }
