09dd44ff2ec232d8c464426c5fddef908ef5ab4e
Jεan Sacren <sakiwit@gmail.com>
2019-10-28 02:54:07 -0600

    Update 4-D driver with Duo class

--- src/main/java/io/github/sacren/Test4D.java
+++ src/main/java/io/github/sacren/Test4D.java
@@ -1,40 +1,50 @@
 /**
- * Test driver for Point, Trio and Quad classes.
+ * Test driver for Duo, Trio and Quad classes.
  */
 public class Test4D {
-    /** Point, Trio and Quad test driver. */
+    /** Duo, Trio and Quad test driver. */
     public static void main(String[] args) {
         /* 2-D */
-        Point p2D = new Point();
+        Duo int2D = new Duo();
         System.out.println();
         System.out.println("=== Default 2-D ===");
         System.out.println();
         System.out.printf(
-                "Default 2-D: P%s%n%n"
-                        + "Default 2-D: P(%.2f, %.2f)%n",
-                p2D,
-                p2D.getX(),
-                p2D.getY());
+                "Default 2-D: %s%n%n"
+                        + "Default 2-D: (%d, %d)%n",
+                int2D,
+                int2D.getFirstInt(),
+                int2D.getSecondInt());
+        Duo dbl2D = new Duo((double) 0, (double) 0);
+        System.out.println();
+        System.out.println("=== 2-D of double ===");
+        System.out.println();
+        System.out.printf(
+                "Custom 2-D: %s%n%n"
+                        + "Custom 2-D: (%f, %f)%n",
+                dbl2D,
+                dbl2D.getFirstDbl(),
+                dbl2D.getSecondDbl());
         /* 3-D */
-        Trio p3D = new Trio();
+        Trio int3D = new Trio();
         System.out.println();
         System.out.println("=== Default 3-D ===");
         System.out.println();
         System.out.printf(
-                "Default 3-D: P%s%n%n"
-                        + "Default 3-D: P(%d, %d, %d)%n",
-                p3D,
-                p3D.getFirstInt(),
-                p3D.getSecondInt(),
-                p3D.getThirdInt());
+                "Default 3-D: %s%n%n"
+                        + "Default 3-D: (%d, %d, %d)%n",
+                int3D,
+                int3D.getFirstInt(),
+                int3D.getSecondInt(),
+                int3D.getThirdInt());
         /* custom 3-D */
         Trio dbl3D = new Trio((double)1, (double)2, (double)3);
         System.out.println();
         System.out.println("=== Custom 3-D ===");
         System.out.println();
         System.out.printf(
-                "Custom 3-D: P%s%n%n"
-                        + "Custom 3-D: P(%.2f, %.2f, %.2f)%n",
+                "Custom 3-D: %s%n%n"
+                        + "Custom 3-D: (%.2f, %.2f, %.2f)%n",
                 dbl3D,
                 dbl3D.getFirstDbl(),
                 dbl3D.getSecondDbl(),
@@ -47,8 +57,8 @@ public class Test4D {
         System.out.println("=== Update 3-D ===");
         System.out.println();
         System.out.printf(
-                "Update 3-D: P%s%n%n"
-                        + "Update 3-D: P(%.2f, %.2f, %.2f)%n",
+                "Update 3-D: %s%n%n"
+                        + "Update 3-D: (%.2f, %.2f, %.2f)%n",
                 dbl3D,
                 dbl3D.getFirstDbl(),
                 dbl3D.getSecondDbl(),
@@ -59,8 +69,8 @@ public class Test4D {
         System.out.println("=== Custom 4-D ===");
         System.out.println();
         System.out.printf(
-                "Custom 4-D: P%s%n%n"
-                        + "Update 4-D: P(%d, %d, %d, %d)%n",
+                "Custom 4-D: %s%n%n"
+                        + "Update 4-D: (%d, %d, %d, %d)%n",
                 int4D,
                 int4D.getFirstInt(),
                 int4D.getSecondInt(),
@@ -75,8 +85,8 @@ public class Test4D {
         int4D.setThird(103);
         int4D.setFourth(104);
         System.out.printf(
-                "Update 4-D: P%s%n%n"
-                        + "Update 4-D: P(%d, %d, %d, %d)%n",
+                "Update 4-D: %s%n%n"
+                        + "Update 4-D: (%d, %d, %d, %d)%n",
                 int4D,
                 int4D.getFirstInt(),
                 int4D.getSecondInt(),
