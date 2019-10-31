0f3a1d31917004961c1e2633ca13494b998e1fe3
Jεan Sacren <sakiwit@gmail.com>
2019-10-31 02:31:43 -0600

    Update interactive message of Line driver

--- src/main/java/io/github/sacren/TestLine.java
+++ src/main/java/io/github/sacren/TestLine.java
@@ -8,21 +8,21 @@ public class TestLine {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double beginX;
-        System.out.print("Enter x-axis for begin point: ");
+        System.out.print("Enter X-axis for begin point: ");
         beginX = in.nextDouble();
         double beginY;
-        System.out.print("Enter y-axis for begin point: ");
+        System.out.print("Enter Y-axis for begin point: ");
         beginY = in.nextDouble();
         double endX;
-        System.out.print("Enter x-axis for end point: ");
+        System.out.print("Enter X-axis for end point: ");
         endX = in.nextDouble();
         double endY;
-        System.out.print("Enter y-axis for end point: ");
+        System.out.print("Enter Y-axis for end point: ");
         endY = in.nextDouble();
         in.close();
         /* use 4 numbers for Line instance */
         System.out.println();
-        System.out.println("=== Line by 4 integers ===");
+        System.out.println("=== Line by 4 numbers ===");
         System.out.println();
         Line line1 = new Line(beginX, beginY, endX, endY);
         System.out.println(line1);
@@ -32,7 +32,7 @@ public class TestLine {
         line1.setEndX(0);
         line1.setEndY(0);
         System.out.println();
-        System.out.println("=== Reset points to 0 ===");
+        System.out.println("=== Reset points to all zeroes ===");
         System.out.println();
         System.out.println(line1);
         /* use points for Line instance */
@@ -54,7 +54,7 @@ public class TestLine {
         line2.setBegin(new Point(0, 0));
         line2.setEnd(new Point(0, 0));
         System.out.println();
-        System.out.println("=== Reset points to 0 by points ===");
+        System.out.println("=== Reset begin and end points to zero ===");
         System.out.println();
         System.out.println(line2);
     }
