cefc1b0e7300dea073387f0571db7405dc531041
Jεan Sacren <sakiwit@gmail.com>
2020-01-02 02:33:24 -0700

    Update Sphere driver interactive message and comment

--- src/main/java/io/github/sacren/TestSphere.java
+++ src/main/java/io/github/sacren/TestSphere.java
@@ -5,12 +5,13 @@ public class TestSphere {
     /** Sphere test driver. */
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("=== Default sphere ===");
+        System.out.println("=== Default sphere not substituted ===");
         System.out.println();
-        /* default sphere of Sphere */
-        System.out.println(new Sphere());
+        /* default sphere object not substituted */
+        Sphere sp = new Sphere();
+        System.out.println(sp);
         System.out.println();
-        System.out.println("=== Default sphere of Shape ===");
+        System.out.println("=== Default sphere substituted by Shape ===");
         System.out.println();
         /* substituted or upcast */
         Shape sp1 = new Sphere();
@@ -18,13 +19,16 @@ public class TestSphere {
         System.out.println();
         System.out.println("=== Update sphere ===");
         System.out.println();
-        /* downcast */
+        /* downcast to Circle */
         Circle sp2 = (Circle) sp1;
         sp2.setRadius(sp2.getRadius() + 1);
         sp2.setColor("red");
         sp2.setCenter(new Point(1, 2));
         System.out.println(sp2);
         System.out.println();
+        System.out.println("=== Update sphere again ===");
+        System.out.println();
+        /* downcast to Sphere */
         Sphere sp3 = (Sphere) sp1;
         sp3.setRadius(sp3.getRadius() + 1);
         sp3.setColor("scarlet");
