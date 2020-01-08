5b2a93a341bab962977d4ae0ee709f1ce4423e30
Jεan Sacren <sakiwit@gmail.com>
2020-01-08 02:22:49 -0700

    Clean up not useful import to Account driver

--- src/main/java/io/github/sacren/TestAccount.java
+++ src/main/java/io/github/sacren/TestAccount.java
@@ -1,8 +1,6 @@
 /**
  * Test driver for Account.
  */
-import static java.lang.System.out;
-
 import java.util.Scanner;
 
 public class TestAccount {
@@ -11,46 +9,46 @@ public class TestAccount {
         /* user input */
         Scanner in = new Scanner(System.in);
         int acctNum;
-        out.print("Enter the account number: ");
+        System.out.print("Enter the account number: ");
         acctNum = in.nextInt();
         double balance;
-        out.print("Enter the balance: ");
+        System.out.print("Enter the balance: ");
         balance = in.nextDouble();
         in.close();
         /* account activities and balance */
         Account defAcct = new Account();
-        out.println();
-        out.println("=== Default Account ===");
-        out.println();
-        out.println(defAcct);
-        out.println();
-        out.println("=== Use getter for default account ===");
-        out.println();
-        out.printf(
+        System.out.println();
+        System.out.println("=== Default Account ===");
+        System.out.println();
+        System.out.println(defAcct);
+        System.out.println();
+        System.out.println("=== Use getter for default account ===");
+        System.out.println();
+        System.out.printf(
                 "Default account number:  %d%n"
                         + "Default account balance: %.2f%n",
                 defAcct.getAcctNum(),
                 defAcct.getBalance());
         Account acct = new Account(acctNum, balance);
-        out.println();
-        out.println("=== Custom Account ===");
-        out.println();
-        out.println(acct);
-        out.println();
-        out.println("=== Reset balance to 0 ===");
-        out.println();
+        System.out.println();
+        System.out.println("=== Custom Account ===");
+        System.out.println();
+        System.out.println(acct);
+        System.out.println();
+        System.out.println("=== Reset balance to 0 ===");
+        System.out.println();
         acct.setBalance(0);
-        out.println(acct);
-        out.println();
-        out.println("=== Deposit $100.00 ===");
-        out.println();
+        System.out.println(acct);
+        System.out.println();
+        System.out.println("=== Deposit $100.00 ===");
+        System.out.println();
         acct.credit(100);
-        out.println(acct);
-        out.println();
-        out.println("=== Withdraw $98.00 ===");
-        out.println();
+        System.out.println(acct);
+        System.out.println();
+        System.out.println("=== Withdraw $98.00 ===");
+        System.out.println();
         acct.debit(98);
-        out.println(acct);
+        System.out.println(acct);
         System.out.println();
         System.out.println("=== Transfer $50 to another account ===");
         System.out.println();
