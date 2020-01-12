cd884b0dad8a9571a07abef051b2d1f88b4e8f4a
Jεan Sacren <sakiwit@gmail.com>
2020-01-12 02:50:59 -0700

    Add Duo test driver

---
+++ src/main/java/io/github/sacren/TestDuo.java
@@ -0,0 +1,43 @@
+/**
+ * Test driver of Duo class.
+ */
+public class TestDuo {
+    /** Duo test driver. */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("=== Default Duo instance ===");
+        System.out.println();
+        System.out.println(new Duo());
+        System.out.println();
+        System.out.println("=== Custom Duo instances ===");
+        System.out.println();
+        System.out.println(new Duo(11, 2));
+        System.out.println(new Duo((double) 11, (double) 2));
+        System.out.println(new Duo(11, (double) 2));
+        System.out.println();
+        System.out.println("=== Mixed Duo instance ===");
+        System.out.println();
+        Duo dbl = new Duo(3, (double) 4);
+        System.out.println(dbl);
+        System.out.println();
+        System.out.println("=== Messed up with double ===");
+        System.out.println();
+        System.out.printf(
+                "(%.2f, %.2f)%n",
+                dbl.getFirstDbl(),
+                dbl.getSecondDbl());
+        System.out.println();
+        System.out.println("=== Duo double string ===");
+        System.out.println();
+        System.out.println(dbl.duoDblStr());
+        System.out.println();
+        System.out.println("=== All Duo instance variables ===");
+        System.out.println();
+        Duo duo = new Duo();
+        System.out.printf(
+                "Integer: (%d, %d)%n"
+                        + "Double:  (%.2f, %.2f)%n",
+                duo.getFirstInt(), duo.getSecondInt(),
+                duo.getFirstDbl(), duo.getSecondDbl());
+    }
+}
