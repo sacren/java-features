71e298b9945b881b0a4b17008565361e0bf5b2af
Jεan Sacren <sakiwit@gmail.com>
2020-01-19 02:08:34 -0700

    Update tests for Duo class

--- src/main/java/io/github/sacren/TestDuo.java
+++ src/main/java/io/github/sacren/TestDuo.java
@@ -7,33 +7,44 @@ public class TestDuo {
         System.out.println();
         System.out.println("=== Default Duo instance ===");
         System.out.println();
-        System.out.println(new Duo());
+        Duo defDuo = new Duo();
+        System.out.println(defDuo);
         System.out.println();
-        System.out.println("=== Custom Duo instances ===");
+        System.out.printf(
+                "Default Duo instance: (%d, %d)%n"
+                        + "                      (%.2f, %.2f)%n",
+                defDuo.getFirstInt(), defDuo.getSecondInt(),
+                defDuo.getFirstDbl(), defDuo.getSecondDbl());
         System.out.println();
-        System.out.println(new Duo(11, 2));
-        System.out.println(new Duo((double) 11, (double) 2));
-        System.out.println(new Duo(11, (double) 2));
+        System.out.println("=== Custom Duo instance ===");
+        System.out.println();
+        Duo intDuo = new Duo(11, 2);
+        System.out.println(intDuo);
+        System.out.println();
+        System.out.printf(
+                "Custom Duo instance: (%d, %d)%n"
+                          + "                     (%.2f, %.2f)%n",
+                intDuo.getFirstInt(), intDuo.getSecondInt(),
+                intDuo.getFirstDbl(), intDuo.getSecondDbl());
+        System.out.println();
+        Duo dblDuo = new Duo((double) 11, 2.0);
+        System.out.println(dblDuo);
+        System.out.println();
+        System.out.printf(
+                "Custom Duo instance: (%d, %d)%n"
+                          + "                     (%.2f, %.2f)%n",
+                dblDuo.getFirstInt(), dblDuo.getSecondInt(),
+                dblDuo.getFirstDbl(), dblDuo.getSecondDbl());
         System.out.println();
         System.out.println("=== Mixed Duo instance ===");
         System.out.println();
-        Duo dbl = new Duo(3, (double) 4);
-        System.out.println(dbl);
-        System.out.println();
-        System.out.println("=== Messed up with double ===");
+        Duo mixed = new Duo(3, (double) 4);
+        System.out.println(mixed);
         System.out.println();
         System.out.printf(
-                "(%.2f, %.2f)%n",
-                dbl.getFirstDbl(),
-                dbl.getSecondDbl());
-        System.out.println();
-        System.out.println("=== All Duo instance variables ===");
-        System.out.println();
-        Duo duo = new Duo();
-        System.out.printf(
-                "Integer: (%d, %d)%n"
-                        + "Double:  (%.2f, %.2f)%n",
-                duo.getFirstInt(), duo.getSecondInt(),
-                duo.getFirstDbl(), duo.getSecondDbl());
+                "Mixed Duo instance: (%d, %d)%n"
+                        + "                    (%.2f, %.2f)%n",
+                mixed.getFirstInt(), mixed.getSecondInt(),
+                mixed.getFirstDbl(), mixed.getSecondDbl());
     }
 }
